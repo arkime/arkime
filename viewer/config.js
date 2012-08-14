7ace988a32607282be299c2c354e83f08fb7a5de
Andy Wick <Andy.Wick@teamaol.com>
2012-08-14 11:39:25 -0400

    Added copyright

--- viewer/config.js
+++ viewer/config.js
@@ -2,6 +2,19 @@
 /* config.js -- Code dealing with the config file, command line arguments, 
  *              and dropping privileges
  *
+ * Copyright 2012 The AOL Moloch Authors.  All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 /*jshint
   node: true, plusplus: false, curly: true, eqeqeq: true, immed: true, latedef: true, newcap: true, nonew: true, undef: true, strict: true, trailing: true
