545082548ecfeca9b0bd8f428f752d2efefc8ea6
Andy Wick <Andy.Wick@teamaol.com>
2012-10-24 12:59:24 -0400

    Updated to support openjdk by default instead of requiring real java

--- single-host/bin/run_es.sh
+++ single-host/bin/run_es.sh
@@ -4,6 +4,7 @@ TDIR=_TDIR_
 
 cd ${TDIR}/elasticsearch-_ES_
 ulimit -a
-export JAVA_OPTS="-XX:+UseCompressedOops"
+# Uncomment if using Sun Java for better memory utilization
+# export JAVA_OPTS="-XX:+UseCompressedOops"
 export ES_HOSTNAME=`hostname -s`a
 ES_HEAP_SIZE=1G bin/elasticsearch -Des.config=${TDIR}/etc/elasticsearch.yml
