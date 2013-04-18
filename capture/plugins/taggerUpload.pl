896e5779940fe671779e1e629fdc2ab9d8bfc977
Andy Wick <Andy.Wick@teamaol.com>
2013-04-17 21:21:58 -0400

    Added v0.8.0 missing files

---
+++ capture/plugins/taggerUpload.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl -w
+# Used to upload new tagger files to the database.  The filename is used as
+# a database key, so make sure to reuse the same filename.
+
+use strict;
+use LWP::UserAgent;
+use Digest::MD5 qw(md5_hex);
+
+sub showHelp($)
+{
+    my ($str) = @_;
+    print $str,"\n";
+    die "$0 ESHOST:ESPORT (ip|host) filename tag1 [tag2..tagN]";
+}
+
+showHelp("Missing arguments") if (@ARGV < 4);
+showHelp("Must be ip or host for file type instead of $ARGV[1]") if ($ARGV[1] ne "ip" && $ARGV[1] ne "host");
+showHelp("file '$ARGV[2]' not found") if (! -f $ARGV[2]);
+showHelp("file '$ARGV[2]' empty") if (-z $ARGV[2]);
+
+my $userAgent = LWP::UserAgent->new(timeout => 20);
+my $response = $userAgent->post("http://$ARGV[0]/tagger",
+                                Content => '{"mappings": {"file": { "_all.enabled" : false, "properties":{"tags":{"type":"string","index": "no"}, "type": {"type":"string","index": "no"}, "data": {"type":"string","index": "no"}, "md5": {"type":"string","index": "no"}}}}}');
+
+my @ELEMENTS;
+open (FILE, $ARGV[2]);
+while (<FILE>) {
+    next if (/^#/ || /^$/);
+    chop;
+    push(@ELEMENTS, $_);
+}
+my $elements = join ',', @ELEMENTS;
+close (FILE);
+
+my $md5hex = md5_hex($elements);
+
+my $content  = '{"tags": "' . join(',', @ARGV[3 .. $#ARGV]) . '", "md5":"' . $md5hex .'", "type":"' . $ARGV[1] . '", "data":"' . $elements . '"}'. "\n";
+#print $content,"\n";
+$response = $userAgent->post("http://$ARGV[0]/tagger/file/$ARGV[2]", Content => $content);
+print $response->content, "\n";
+
