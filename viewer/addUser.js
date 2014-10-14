7cd0832b6922517e4fbba8eba329fd324acf7ae5
Andy Wick <Andy.Wick@teamaol.com>
2014-10-13 22:54:27 -0400

    Fixed addUser to work with new Db.initialize

--- viewer/addUser.js
+++ viewer/addUser.js
@@ -27,7 +27,7 @@ var Db = require ("./db.js");
 var crypto = require('crypto');
 
 var escInfo = Config.get("elasticsearch", "http://localhost:9200");
-Db.initialize({host : escInfo});
+Db.initialize({host : escInfo}, main);
 
 function help() {
   console.log("addUser.js <user id> <user friendly name> <password> [<options>]");
@@ -42,62 +42,64 @@ function help() {
   process.exit(0);
 }
 
-if (process.argv.length < 5) {
-  help();
-}
-
-var nuser = {
-  userId: process.argv[2],
-  userName: process.argv[3],
-  passStore: Config.pass2store(process.argv[2], process.argv[4]),
-  enabled: true,
-  webEnabled: true,
-  headerAuthEnabled: false,
-  emailSearch: false,
-  createEnabled: false,
-  removeEnabled: false
-};
-
-var i;
-for (i = 5; i < process.argv.length; i++) {
-  switch(process.argv[i]) {
-  case "--admin":
-  case "-admin":
-    nuser.createEnabled = true;
-    break;
-
-  case "--remove":
-  case "-remove":
-    nuser.removeEnabled = true;
-    break;
-
-  case "--noweb":
-  case "-noweb":
-  case "--apionly":
-    nuser.webEnabled = false;
-    break;
-    
-  case "--webauth":
-  case "-webauth":
-    nuser.headerAuthEnabled = true;
-    break;
-
-  case "--email":
-  case "-email":
-    nuser.emailSearch = true;
-    break;
-
-  default:
-    console.log("Unknown option", process.argv[i]);
+function main() {
+  if (process.argv.length < 5) {
     help();
   }
-}
 
-Db.indexNow("users", "user", process.argv[2], nuser, function(err, info) {
-  if (err) {
-    console.log("Elastic search error", err);
-  } else {
-    console.log("Added");
+  var nuser = {
+    userId: process.argv[2],
+    userName: process.argv[3],
+    passStore: Config.pass2store(process.argv[2], process.argv[4]),
+    enabled: true,
+    webEnabled: true,
+    headerAuthEnabled: false,
+    emailSearch: false,
+    createEnabled: false,
+    removeEnabled: false
+  };
+
+  var i;
+  for (i = 5; i < process.argv.length; i++) {
+    switch(process.argv[i]) {
+    case "--admin":
+    case "-admin":
+      nuser.createEnabled = true;
+      break;
+
+    case "--remove":
+    case "-remove":
+      nuser.removeEnabled = true;
+      break;
+
+    case "--noweb":
+    case "-noweb":
+    case "--apionly":
+      nuser.webEnabled = false;
+      break;
+      
+    case "--webauth":
+    case "-webauth":
+      nuser.headerAuthEnabled = true;
+      break;
+
+    case "--email":
+    case "-email":
+      nuser.emailSearch = true;
+      break;
+
+    default:
+      console.log("Unknown option", process.argv[i]);
+      help();
+    }
   }
-  Db.close();
-});
+
+  Db.indexNow("users", "user", process.argv[2], nuser, function(err, info) {
+    if (err) {
+      console.log("Elastic search error", err);
+    } else {
+      console.log("Added");
+    }
+    Db.close();
+  });
+}
