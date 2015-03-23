a0c6c865f3bcd3336407220462858d10cfa38d31
William Salusky <wsalusky@salesforce.com>
2015-03-23 16:31:26 +0000

    start with symlink node-viewer for proc mgmgt

--- single-host/bin/run_viewer.sh
+++ single-host/bin/run_viewer.sh
@@ -8,4 +8,4 @@ cd ${TDIR}/viewer
 /bin/rm -f ${TDIR}/logs/viewer.log.old
 /bin/mv ${TDIR}/logs/viewer.log ${TDIR}/logs/viewer.log.old
 export NODE_ENV=production 
-exec ${TDIR}/bin/node viewer.js -c ${TDIR}/etc/config.ini > ${TDIR}/logs/viewer.log 2>&1
+exec ${TDIR}/bin/node-viewer viewer.js -c ${TDIR}/etc/config.ini > ${TDIR}/logs/viewer.log 2>&1
