de23456555c4a5e2d9337c5f8c602a828e51969b
Andy Wick <Andy.Wick@teamaol.com>
2016-04-09 16:40:38 -0400

    writer simple now uses a thread

--- capture/writer-simple.c
+++ capture/writer-simple.c
@@ -1,7 +1,8 @@
 /******************************************************************************/
 /* writer-simple.c  -- Simple Writer Plugin
  *
- * This writer just creates a file per thread and writes to it, no locks
+ * This writer just creates a file per packet thread and queues buffers
+ * to be written to disk in a single output thread.
  *
  * Copyright 2012-2016 AOL Inc. All rights reserved.
  *
@@ -30,69 +31,66 @@
 extern MolochConfig_t        config;
 extern MolochPcapFileHdr_t   pcapFileHeader;
 
-typedef struct  {
-    char      *buf;
-    pthread_t  self;
-    char      *name;
-    uint64_t   pos;
-    uint32_t   id;
-    int        fd;
-    uint32_t   bufpos;
+typedef struct molochsimple {
+    struct molochsimple *simple_next, *simple_prev;
+    int                  simple_count;
+    char                *buf;
+    uint64_t             pos;
+    uint32_t             id;
+    int                  fd;
+    uint32_t             bufpos;
+    int                  closing;
 } MolochSimple_t;
 
-LOCAL MolochSimple_t         info[MOLOCH_MAX_PACKET_THREADS];
+static MolochSimple_t    simpleQ;
+static MOLOCH_LOCK_DEFINE(simpleQ);
+static MOLOCH_COND_DEFINE(simpleQ);
+
+LOCAL MolochSimple_t        *currentInfo[MOLOCH_MAX_PACKET_THREADS];
 LOCAL int                    pageSize;
 
 /******************************************************************************/
 uint32_t writer_simple_queue_length()
 {
-    return 0;
+    return DLL_COUNT(simple_, &simpleQ);
 }
 /******************************************************************************/
-/* If closing is true the buffer can be any size, if false then has to be pcapWriteSize */
-void writer_simple_write_buf(int thread, int closing)
+MolochSimple_t *writer_simple_alloc(MolochSimple_t *previous)
 {
-    uint32_t pos = 0;
-    uint32_t total;
-    if (closing) {
-        total = info[thread].bufpos;
-        if (total % pageSize != 0) {
-            total = (total - (total % pageSize) + pageSize);
-        }
-    } else {
-        total = config.pcapWriteSize;
-    }
+    MolochSimple_t *info;
 
-    while (pos < total) {
-        int len = write(info[thread].fd, info[thread].buf + pos, total-pos);
-        if (len >= 0) {
-            pos += len;
-        } else {
-            LOG("ERROR writing - %d %s", len, strerror(errno));
-        }
+    info = MOLOCH_TYPE_ALLOC0(MolochSimple_t);
+    info->buf = mmap (0, config.pcapWriteSize + MOLOCH_PACKET_MAX_LEN, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+    if (previous) {
+        info->fd = previous->fd;
+        info->pos = previous->pos;
     }
-    if (closing) {
-        ftruncate(info[thread].fd, info[thread].pos);
-        info[thread].bufpos = 0;
-    } else {
-        memmove(info[thread].buf, info[thread].buf + total, info[thread].bufpos - total);
-        info[thread].bufpos = info[thread].bufpos - total;
-    }
+    return info;
 }
 /******************************************************************************/
-void writer_simple_exit()
+void writer_simple_free(MolochSimple_t *info)
 {
-    int thread;
+    munmap(info->buf, config.pcapWriteSize + MOLOCH_PACKET_MAX_LEN);
+    MOLOCH_TYPE_FREE(MolochSimple_t, info);
+}
 
-    for (thread = 0; thread < config.packetThreads; thread++) {
-        if (info[thread].fd) {
-            writer_simple_write_buf(thread, 1);
-            close(info[thread].fd);
-            info[thread].fd = 0;
-            g_free(info[thread].name);
-        }
-        munmap(info[thread].buf, config.pcapWriteSize + MOLOCH_PACKET_MAX_LEN);
+/******************************************************************************/
+void writer_simple_process_buf(int thread, int closing)
+{
+    MolochSimple_t *info = currentInfo[thread];
+
+    info->closing = closing;
+    if (!closing) {
+        currentInfo[thread] = writer_simple_alloc(info);
+        memcpy(currentInfo[thread]->buf, info->buf + config.pcapWriteSize, info->bufpos - config.pcapWriteSize);
+        currentInfo[thread]->bufpos = info->bufpos - config.pcapWriteSize;
+    } else {
+        currentInfo[thread] = NULL;
     }
+    MOLOCH_LOCK(simpleQ);
+    DLL_PUSH_TAIL(simple_, &simpleQ, info);
+    MOLOCH_UNLOCK(simpleQ);
+    MOLOCH_COND_BROADCAST(simpleQ);
 }
 /******************************************************************************/
 struct pcap_timeval {
@@ -108,33 +106,29 @@ void writer_simple_write(const MolochSession_t * const session, MolochPacket_t *
 {
     int thread = session->thread;
 
-    if (info[thread].self == 0)
-        info[thread].self = pthread_self();
-
-    if (info[thread].self != pthread_self()) {
-        LOG("ERROR - Writing on wrong thread");
-        exit(3);
-    }
-
-    if (!info[thread].fd) {
-        info[thread].name = moloch_db_create_file(packet->ts.tv_sec, NULL, 0, 0, &info[thread].id);
+    if (!currentInfo[thread]) {
+        currentInfo[thread] = writer_simple_alloc(NULL);
+        char *name = moloch_db_create_file(packet->ts.tv_sec, NULL, 0, 0, &currentInfo[thread]->id);
         int options = O_NOATIME | O_WRONLY | O_CREAT | O_TRUNC;
 #ifdef O_DIRECT
         options |= O_DIRECT;
+#else
+        LOG("No O_DIRECT");
 #endif
-        info[thread].fd = open(info[thread].name,  options, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-        if (info[thread].fd < 0) {
-            LOG("ERROR - pcap open failed - Couldn't open file: '%s' with %s  (%d)", info[thread].name, strerror(errno), errno);
+        currentInfo[thread]->fd = open(name,  options, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+        if (currentInfo[thread]->fd < 0) {
+            LOG("ERROR - pcap open failed - Couldn't open file: '%s' with %s  (%d)", name, strerror(errno), errno);
             exit(2);
         }
-        info[thread].pos = info[thread].bufpos = 24;
-        memcpy(info[thread].buf, &pcapFileHeader, 24);
+        currentInfo[thread]->pos = currentInfo[thread]->bufpos = 24;
+        memcpy(currentInfo[thread]->buf, &pcapFileHeader, 24);
         if (config.debug)
-            LOG("opened %d %s %d", thread, info[thread].name, info[thread].fd);
+            LOG("opened %d %s %d", thread, name, currentInfo[thread]->fd);
+        g_free(name);
     }
 
-    packet->writerFileNum = info[thread].id;
-    packet->writerFilePos = info[thread].pos;
+    packet->writerFileNum = currentInfo[thread]->id;
+    packet->writerFilePos = currentInfo[thread]->pos;
 
     struct pcap_sf_pkthdr hdr;
 
@@ -143,21 +137,75 @@ void writer_simple_write(const MolochSession_t * const session, MolochPacket_t *
     hdr.caplen     = packet->pktlen;
     hdr.pktlen     = packet->pktlen;
 
-    memcpy(info[thread].buf+info[thread].bufpos, &hdr, 16);
-    info[thread].bufpos += 16;
-    memcpy(info[thread].buf+info[thread].bufpos, packet->pkt, packet->pktlen);
-    info[thread].bufpos += packet->pktlen;
-    info[thread].pos += 16 + packet->pktlen;
+    memcpy(currentInfo[thread]->buf+currentInfo[thread]->bufpos, &hdr, 16);
+    currentInfo[thread]->bufpos += 16;
+    memcpy(currentInfo[thread]->buf+currentInfo[thread]->bufpos, packet->pkt, packet->pktlen);
+    currentInfo[thread]->bufpos += packet->pktlen;
+    currentInfo[thread]->pos += 16 + packet->pktlen;
 
-    if (info[thread].bufpos > config.pcapWriteSize) {
-        writer_simple_write_buf(thread, 0);
+    if (currentInfo[thread]->bufpos > config.pcapWriteSize) {
+        writer_simple_process_buf(thread, 0);
+    } else if (currentInfo[thread]->pos >= config.maxFileSizeB) {
+        writer_simple_process_buf(thread, 1);
+    }
+}
+/******************************************************************************/
+void *writer_simple_thread(void *UNUSED(arg))
+{
+    MolochSimple_t *info;
+
+    LOG("THREAD %p", (gpointer)pthread_self());
+    while (1) {
+        MOLOCH_LOCK(simpleQ);
+        while (DLL_COUNT(simple_, &simpleQ) == 0) {
+            MOLOCH_COND_WAIT(simpleQ);
+        }
+        DLL_POP_HEAD(simple_, &simpleQ, info);
+        MOLOCH_UNLOCK(simpleQ);
+
+        uint32_t pos = 0;
+        uint32_t total;
+        if (info->closing) {
+            total = info->bufpos;
+            if (total % pageSize != 0) {
+                total = (total - (total % pageSize) + pageSize);
+            }
+        } else {
+            total = config.pcapWriteSize;
+        }
+
+        while (pos < total) {
+            int len = write(info->fd, info->buf + pos, total - pos);
+            if (len >= 0) {
+                pos += len;
+            } else {
+                LOG("ERROR writing - %d %s", len, strerror(errno));
+                exit(0);
+            }
+        }
+        if (info->closing) {
+            ftruncate(info->fd, info->pos);
+            close(info->fd);
+        }
+
+        writer_simple_free(info);
+    }
+    return NULL;
+}
+/******************************************************************************/
+void writer_simple_exit()
+{
+    int thread;
+
+    for (thread = 0; thread < config.packetThreads; thread++) {
+        if (currentInfo[thread]) {
+            writer_simple_process_buf(thread, 1);
+        }
     }
 
-    if (info[thread].pos >= config.maxFileSizeB) {
-        writer_simple_write_buf(thread, 1);
-        close(info[thread].fd);
-        info[thread].fd = 0;
-        g_free(info[thread].name);
+    // Pause the main thread until our thread finishes
+    while (writer_simple_queue_length() > 0) {
+        usleep(10000);
     }
 }
 /******************************************************************************/
@@ -173,9 +221,6 @@ void writer_simple_init(char *UNUSED(name))
         LOG ("INFO: Reseting pcapWriteSize to %u since it must be a multiple of %u", config.pcapWriteSize, pageSize);
     }
 
-    int thread;
-    for (thread = 0; thread < config.packetThreads; thread++) {
-        info[thread].buf = mmap (0, config.pcapWriteSize + MOLOCH_PACKET_MAX_LEN, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
-    }
-
+    DLL_INIT(simple_, &simpleQ);
+    g_thread_new("moloch-simple", &writer_simple_thread, NULL);
 }
