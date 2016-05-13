a4bb9da1872062f7059085c8385851b4ef30699a
Andy Wick <andywick@gmail.com>
2016-05-13 14:44:03 -0400

    pcre raw mode, fix var names

--- capture/plugins/lua/data.c
+++ capture/plugins/lua/data.c
@@ -19,51 +19,51 @@
 /******************************************************************************/
 static MD_t *checkMolochData (lua_State *L, int index)
 {
-    MD_t *ms;
+    MD_t *md;
     luaL_checktype(L, index, LUA_TUSERDATA);
-    ms = (MD_t*)luaL_checkudata(L, index, "MolochData");
-    if (ms == NULL) {
+    md = (MD_t*)luaL_checkudata(L, index, "MolochData");
+    if (md == NULL) {
         luaL_argerror(L, index, lua_pushfstring(L, "MolochData expected, got %s", luaL_typename(L, index)));
         return NULL;
     }
-    return ms;
+    return md;
 }
 /******************************************************************************/
 MD_t *molua_pushMolochData (lua_State *L, const char *str, int len)
 {
-    MD_t *ms = (MD_t *)lua_newuserdata(L, sizeof(MD_t));
-    ms->str = str;
-    ms->len = len;
-    ms->needFree = 0;
+    MD_t *md = (MD_t *)lua_newuserdata(L, sizeof(MD_t));
+    md->str = str;
+    md->len = len;
+    md->needFree = 0;
     luaL_getmetatable(L, "MolochData");
     lua_setmetatable(L, -2);
-    return ms;
+    return md;
 }
 /******************************************************************************/
 int MD_tostring(lua_State *L)
 {
-    MD_t *ms = checkMolochData(L, 1);
-    lua_pushlstring(L, ms->str, ms->len);
+    MD_t *md = checkMolochData(L, 1);
+    lua_pushlstring(L, md->str, md->len);
     return 1;
 }
 /******************************************************************************/
 int MD_gc(lua_State *L)
 {
-    MD_t *ms = checkMolochData(L, 1);
-    if (ms->needFree) {
-        g_free((gpointer)ms->str);
+    MD_t *md = checkMolochData(L, 1);
+    if (md->needFree) {
+        g_free((gpointer)md->str);
     }
     return 0;
 }
 /******************************************************************************/
 int MD_memmem(lua_State *L)
 {
-    MD_t *ms = checkMolochData(L, 1);
+    MD_t *md = checkMolochData(L, 1);
     size_t len;
     const char *needle = luaL_checklstring(L, 2, &len);
-    const char *match = moloch_memstr(ms->str, ms->len, needle, len);
+    const char *match = moloch_memstr(md->str, md->len, needle, len);
     if (match) {
-        lua_pushinteger(L, match - ms->str);
+        lua_pushinteger(L, match - md->str);
     } else {
         lua_pushinteger(L, -1);
     }
@@ -72,10 +72,10 @@ int MD_memmem(lua_State *L)
 /******************************************************************************/
 int MD_copy(lua_State *L)
 {
-    MD_t *ms = checkMolochData(L, 1);
-    MD_t *nms = molua_pushMolochData(L, ms->str, ms->len);
-    nms->str = g_memdup(ms->str, ms->len);
-    nms->needFree = 1;
+    MD_t *md = checkMolochData(L, 1);
+    MD_t *nmd = molua_pushMolochData(L, md->str, md->len);
+    nmd->str = g_memdup(md->str, md->len);
+    nmd->needFree = 1;
     return 1;
 }
 /******************************************************************************/
@@ -83,16 +83,16 @@ int MD_new(lua_State *L)
 {
     size_t len;
     const char *str = luaL_checklstring(L, 1, &len);
-    MD_t *ms = molua_pushMolochData(L, str, len);
-    ms->str = g_memdup(str, len);
-    ms->needFree = 1;
+    MD_t *md = molua_pushMolochData(L, str, len);
+    md->str = g_memdup(str, len);
+    md->needFree = 1;
     return 1;
 }
 /******************************************************************************/
 int MD_pcre_create(lua_State *L)
 {
     const char *str = luaL_checkstring(L, 1);
-    GRegex     *regex = g_regex_new(str, 0, 0, 0);
+    GRegex     *regex = g_regex_new(str, G_REGEX_RAW, 0, 0);
     lua_pushlightuserdata(L, regex);
     return 1;
 }
