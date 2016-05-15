b1aab71811914b9258f9436efc99eead67ef98a5
Andy Wick <andywick@gmail.com>
2016-05-15 13:16:24 -0400

    more ldap and new radius support

--- capture/parsers/ldap.c
+++ capture/parsers/ldap.c
@@ -16,6 +16,9 @@
 
 extern MolochConfig_t        config;
 
+static int bindNameField;
+static int authTypeField;
+
 typedef struct {
     unsigned char       buf[2][8192];
     int                 len[2];
@@ -23,39 +26,79 @@ typedef struct {
 /******************************************************************************/
 void ldap_process(MolochSession_t *session, LDAPInfo_t *ldap, int which)
 {
-    BSB bsb;
-    BSB_INIT(bsb, ldap->buf[which], ldap->len[which]);
+    BSB obsb, ibsb;
     uint32_t opc, otag, olen;
     uint32_t ipc, itag, ilen;
     uint32_t protocolOp;
     unsigned char *ovalue, *ivalue;
 
-    ovalue = moloch_parsers_asn_get_tlv(&bsb, &opc, &otag, &olen);
+    BSB_INIT(obsb, ldap->buf[which], ldap->len[which]);
+    ldap->len[which] = -1; // stop any calls for this direction
 
-    BSB_INIT(bsb, ovalue, olen);
+    while (BSB_REMAINING(obsb) > 5) {
+        ovalue = moloch_parsers_asn_get_tlv(&obsb, &opc, &otag, &olen);
 
-    // messageID
-    ivalue = moloch_parsers_asn_get_tlv(&bsb, &ipc, &itag, &ilen);
-    if (ipc != 0 || itag != 2)
-        return;
+        BSB_INIT(ibsb, ovalue, olen);
 
-    // protocolOp
-    ivalue = moloch_parsers_asn_get_tlv(&bsb, &ipc, &protocolOp, &ilen);
-    if (ipc != 1 || protocolOp > 25)
-        return;
+        // messageID
+        ivalue = moloch_parsers_asn_get_tlv(&ibsb, &ipc, &itag, &ilen);
+        if (ipc != 0 || itag != 2)
+            return;
 
-    if (protocolOp == 23) {
-        int len = ldap->len[which] - olen - 2;
-        ldap->len[which] = -1;
-        moloch_parsers_classify_tcp(session, ldap->buf[which] + olen + 2, len, which);
-        moloch_packet_process_data(session, ldap->buf[which] + olen + 2, len, which);
-    } else if (protocolOp == 24) {
-        int len = ldap->len[which] - olen - 2;
-        ldap->len[which] = -1;
-        moloch_packet_process_data(session, ldap->buf[which] + olen + 2, len, which);
-        moloch_parsers_unregister(session, ldap);
-    } else {
-        moloch_parsers_unregister(session, ldap);
+        // protocolOp
+        ivalue = moloch_parsers_asn_get_tlv(&ibsb, &ipc, &protocolOp, &ilen);
+        if (ipc != 1 || protocolOp > 25)
+            return;
+
+        if (protocolOp == 0) {
+            BSB_INIT(ibsb, ivalue, ilen);
+            ivalue = moloch_parsers_asn_get_tlv(&ibsb, &ipc, &itag, &ilen); // version
+            if (!ivalue)
+                continue;
+
+            ivalue = moloch_parsers_asn_get_tlv(&ibsb, &ipc, &itag, &ilen); // name
+            if (!ivalue)
+                continue;
+            if (ilen == 0) {
+                moloch_field_string_add(bindNameField, session, "<ROOT>", 6, TRUE);
+            } else {
+                moloch_field_string_add(bindNameField, session, (const char*)ivalue, ilen, TRUE);
+            }
+            ivalue = moloch_parsers_asn_get_tlv(&ibsb, &ipc, &itag, &ilen); // auth
+            if (!ivalue)
+                continue;
+
+            char str[100];
+            switch (itag) {
+            case 0:
+                if (ilen == 0)
+                    moloch_field_string_add(authTypeField, session, "none", 4, TRUE);
+                else
+                    moloch_field_string_add(authTypeField, session, "simple", 6, TRUE);
+                break;
+            case 3:
+                moloch_field_string_add(authTypeField, session, "sasl", 4, TRUE);
+                break;
+            case 10:
+                moloch_field_string_add(authTypeField, session, "ntlmsspNegotiate", 16, TRUE); // from wireshark
+                break;
+            case 11:
+                moloch_field_string_add(authTypeField, session, "ntlmsspAuth", 11, TRUE); // from wireshark
+                break;
+            default:
+                sprintf(str, "%d", itag);
+                moloch_field_string_add(authTypeField, session, str, -1, TRUE);
+
+            }
+        } else if (protocolOp == 23) {
+            int len = BSB_SIZE(obsb) - olen - 2;
+            moloch_parsers_classify_tcp(session, ldap->buf[which] + olen + 2, len, which);
+            moloch_packet_process_data(session, ldap->buf[which] + olen + 2, len, which);
+            return;
+        } else if (protocolOp == 24) {
+            int len = BSB_SIZE(obsb) - olen - 2;
+            moloch_packet_process_data(session, ldap->buf[which] + olen + 2, len, which);
+        }
     }
 }
 /******************************************************************************/
@@ -71,8 +114,11 @@ int ldap_parser(MolochSession_t *session, void *uw, const unsigned char *data, i
     memcpy(ldap->buf[which] + ldap->len[which], data, MIN(remaining, (int)sizeof(ldap->buf[which])-ldap->len[which]));
     ldap->len[which] += MIN(remaining, (int)sizeof(ldap->buf[which])-ldap->len[which]);
 
-    if (ldap->len[which] > 6000)
+    if (ldap->len[which] > 6000) {
         ldap_process(session, ldap, which);
+        if (ldap->len[(which + 1) %2] == -1) // If other direction is finished then unregister
+            moloch_parsers_unregister(session, ldap);
+    }
 
     return 0;
 }
@@ -83,6 +129,9 @@ void ldap_save(MolochSession_t *session, void *uw, int UNUSED(final))
 
     if (ldap->len[0] > 5) {
         ldap_process(session, ldap, 0);
+    }
+
+    if (ldap->len[1] > 5) {
         ldap_process(session, ldap, 1);
     }
 }
@@ -130,4 +179,16 @@ void moloch_parser_init()
 {
     moloch_parsers_classifier_register_tcp("ldap", NULL, 0, (unsigned char*)"\x30", 1, ldap_classify);
     moloch_parsers_classifier_register_udp("ldap", NULL, 0, (unsigned char*)"\x30", 1, ldap_classify);
+
+    authTypeField = moloch_field_define("ldap", "termfield",
+        "ldap.authtype", "Auth Type", "ldap.authtype-term",
+        "The auth type of ldap bind",
+        MOLOCH_FIELD_TYPE_STR_HASH,  MOLOCH_FIELD_FLAG_COUNT,
+        NULL);
+
+    bindNameField = moloch_field_define("ldap", "termfield",
+        "ldap.bindname", "Auth Type", "ldap.bindname-term",
+        "The bind name of ldap bind",
+        MOLOCH_FIELD_TYPE_STR_HASH,  MOLOCH_FIELD_FLAG_COUNT,
+        NULL);
 }
---
+++ capture/parsers/radius.c
@@ -0,0 +1,117 @@
+/* Copyright 2012-2016 AOL Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this Software except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "moloch.h"
+#include <arpa/inet.h>
+
+extern MolochConfig_t        config;
+static int userField;
+static int macField;
+static int endpointIpField;
+
+/******************************************************************************/
+int radius_udp_parser(MolochSession_t *session, void *UNUSED(uw), const unsigned char *data, int len, int UNUSED(which))
+{
+    BSB bsb;
+
+    BSB_INIT(bsb, data, len);
+    int code;
+
+    BSB_IMPORT_u08(bsb, code);
+    BSB_IMPORT_skip(bsb, 19);
+
+    int type, length;
+    unsigned char *value;
+    char str[100];
+    struct in_addr in;
+
+    while (BSB_REMAINING(bsb) > 2) {
+        BSB_IMPORT_u08(bsb, type);
+        BSB_IMPORT_u08(bsb, length);
+        BSB_IMPORT_ptr(bsb, value, length-2);
+        switch (type) {
+        case 1:
+            moloch_field_string_add(userField, session, (char *)value, length-2, TRUE);
+            break;
+    /*    case 4:
+            LOG("NAS-IP-Address: %d %d %u.%u.%u.%u", type, length, value[0], value[1], value[2], value[3]);
+            break;*/
+        case 31:
+            if (length == 14) {
+                sprintf(str, "%c%c:%c%c:%c%c:%c%c:%c%c:%c%c",
+                        value[2], value[3],
+                        value[4], value[5],
+                        value[6], value[7],
+                        value[8], value[9],
+                        value[10], value[11],
+                        value[12], value[13]);
+                moloch_field_string_add(macField, session, str, 17, TRUE);
+            }
+        case 66:
+            memcpy(str, value, length-2);
+            str[length-2] = 0;
+            inet_aton(str, &in);
+            moloch_field_int_add(endpointIpField, session, in.s_addr);
+            break;
+
+/*        default:
+            LOG("%d %d %.*s", type, length, length-2, value);*/
+        }
+    }
+    return 0;
+}
+/******************************************************************************/
+void radius_udp_classify(MolochSession_t *session, const unsigned char *UNUSED(data), int len, int UNUSED(which), void *UNUSED(uw))
+{
+    if (len != ((data[2] << 8) | data[3])) {
+        return;
+    }
+
+    if (session->port1 >= 1812 || session->port1 <= 1813 ||
+        session->port1 >= 1645 || session->port1 <= 1646 ||
+        session->port2 >= 1812 || session->port2 <= 1813 ||
+        session->port2 >= 1645 || session->port2 <= 1646) {
+        moloch_parsers_register(session, radius_udp_parser, 0, 0);
+        moloch_session_add_protocol(session, "radius");
+    }
+}
+/******************************************************************************/
+void moloch_parser_init()
+{
+    userField = moloch_field_define("radius", "termfield",
+        "radius.user", "User", "radius.user-term",
+        "RADIUS user",
+        MOLOCH_FIELD_TYPE_STR_HASH,     0, 
+        "category", "user",
+        NULL);
+
+    macField = moloch_field_define("radius", "lotermfield",
+        "radius.mac", "MAC", "mac-term",
+        "Radius Mac",
+        MOLOCH_FIELD_TYPE_STR_HASH,  MOLOCH_FIELD_FLAG_COUNT,
+        NULL); 
+
+    endpointIpField = moloch_field_define("radius", "ip",
+        "radius.endpointIp", "Endpoint IP", "radius.ip",
+        "Radius Endpoint ip addresses for session",
+        MOLOCH_FIELD_TYPE_IP_GHASH,  MOLOCH_FIELD_FLAG_COUNT,
+        NULL);
+
+
+    moloch_parsers_classifier_register_udp("radius", NULL, 0, (const unsigned char *)"\x01", 1, radius_udp_classify);
+    moloch_parsers_classifier_register_udp("radius", NULL, 0, (const unsigned char *)"\x02", 1, radius_udp_classify);
+    moloch_parsers_classifier_register_udp("radius", NULL, 0, (const unsigned char *)"\x03", 1, radius_udp_classify);
+    moloch_parsers_classifier_register_udp("radius", NULL, 0, (const unsigned char *)"\x04", 1, radius_udp_classify);
+    moloch_parsers_classifier_register_udp("radius", NULL, 0, (const unsigned char *)"\x05", 1, radius_udp_classify);
+}
