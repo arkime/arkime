022dd3a208b43e2e77f22bdf56ce913e2ca7ede3
Andy Wick <Andy.Wick@teamaol.com>
2016-05-17 16:06:13 -0400

    ES 2.2.2, run ES as daemon, new config

--- single-host/bin/run_es.sh
+++ single-host/bin/run_es.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-TDIR=_TDIR_
+USER=daemon
 
-cd ${TDIR}/elasticsearch-_ES_
 ulimit -a
-# Uncomment if using Sun Java for better memory utilization
-# export JAVA_OPTS="-XX:+UseCompressedOops"
 export ES_HOSTNAME=`hostname -s`a
+export ES_HEAP_SIZE=_ESMEM_
+export ES_NODE_MASTER=true
+export ES_DATA_DIR=_TDIR_/data
+export ES_LOG_DIR=_TDIR_/logs
 
-# Increase memory
-ES_HEAP_SIZE=_ESMEM_ bin/elasticsearch -Des.config=${TDIR}/etc/elasticsearch.yml -d
+chown -R $USER $ES_DATA_DIR $ES_LOG_DIR
+
+exec su -s /bin/sh -c 'exec "$0" "$@" > /dev/null' $USER -- _TDIR_/elasticsearch-_ES_/bin/elasticsearch -Des.default.path.conf=_TDIR_/etc -d
