6867d7e916b5bf1453b943c1ce6c4d758c6e7247
MattCarothers <matt.carothers@cox.com>
2016-09-08 15:06:54 -0400

    Adds two new algorithms for selecting pcap directories when pcapDir has multiple values. (#529)
    Adds a new config file key "pcapDirAlgorithm."  Valid values:
    
    - round-robin: uses each directory in sequence and then starts over at the beginning of the list
    - max-free-percent: chooses the directory on the filesystem with the highest percentage of free disk space
    - max-free-bytes: chooses the directory on the filesystem with the highest number of available bytes
    
    The default is "round-robin."

--- capture/moloch.h
+++ capture/moloch.h
@@ -338,6 +338,7 @@ typedef struct moloch_config {
     char      compressES;
     char      antiSynDrop;
     char      readTruncatedPackets;
+    char     *pcapDirAlgorithm;
 } MolochConfig_t;
 
 typedef struct {
