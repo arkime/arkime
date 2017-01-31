762ac19dc632ec296478ded3740e1dfc44c54f23
Andy Wick <andywick@gmail.com>
2017-01-31 11:54:05 -0500

    Use $@

--- release/moloch_add_user.sh
+++ release/moloch_add_user.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/data/BUILD_MOLOCH_NAME/bin/node /data/BUILD_MOLOCH_NAME/viewer/addUser.js -c /data/BUILD_MOLOCH_NAME/etc/config.ini $*
+/data/BUILD_MOLOCH_NAME/bin/node /data/BUILD_MOLOCH_NAME/viewer/addUser.js -c /data/BUILD_MOLOCH_NAME/etc/config.ini "$@"
