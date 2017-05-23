4a76b10492f61e1daca1f9a8298c1b38aa76380d
Andy Wick <andywick@gmail.com>
2017-05-23 14:17:11 -0400

    Average/Total too slow for now, will require ES 5

--- viewer/app/modules/stats/stats.component.js
+++ viewer/app/modules/stats/stats.component.js
@@ -316,8 +316,6 @@
         for (var i = 0, ilen = nodes.length; i < ilen; i++) {
           metrics.push(metric(nodes[i]));
         }
-        metrics.push(metric("Average"));
-        metrics.push(metric("Total"));
 
         div.append('div')
           .attr('class', 'axis')
