429dd4792d8b4a096d7a0d5e2f476541cab7c19c
bladeswords <bladeswords@users.noreply.github.com>
2017-07-18 00:53:52 +1000

    Added check for sudo, fixed formatting.
    Added a check for the presence of sudo
    Thanks - https://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
    
    Made error format a little more consistent.

--- easybutton-build.sh
+++ easybutton-build.sh
@@ -51,6 +51,8 @@ do
   esac
 done
 
+# Check the existance of sudo
+command -v sudo >/dev/null 2>&1 || { echo >&2 "MOLOCH: sudo is required to be installed"; exit 1; }
 
 MAKE=make
 
@@ -59,15 +61,15 @@ echo "MOLOCH: Installing Dependencies"
 if [ -f "/etc/redhat-release" ]; then
   sudo yum -y install wget curl pcre pcre-devel pkgconfig flex bison gcc-c++ zlib-devel e2fsprogs-devel openssl-devel file-devel make gettext libuuid-devel perl-JSON bzip2-libs bzip2-devel perl-libwww-perl libpng-devel xz libffi-devel readline-devel libtool libyaml-devel
   if [ $? -ne 0 ]; then
-    echo "MOLOCH - yum failed"
+    echo "MOLOCH: yum failed"
     exit 1
   fi
 fi
 
 if [ -f "/etc/debian_version" ]; then
-  sudo apt-get -y install wget curl libpcre3-dev uuid-dev libmagic-dev pkg-config g++ flex bison zlib1g-dev libffi-dev gettext libgeoip-dev make libjson-perl libbz2-dev libwww-perl libpng-dev xz-utils libffi-dev libssl-dev libreadline-dev libtool libyaml-dev sudo dh-autoreconf
+  sudo apt-get -y install wget curl libpcre3-dev uuid-dev libmagic-dev pkg-config g++ flex bison zlib1g-dev libffi-dev gettext libgeoip-dev make libjson-perl libbz2-dev libwww-perl libpng-dev xz-utils libffi-dev libssl-dev libreadline-dev libtool libyaml-dev dh-autoreconf
   if [ $? -ne 0 ]; then
-    echo "MOLOCH - apt-get failed"
+    echo "MOLOCH: apt-get failed"
     exit 1
   fi
 fi
