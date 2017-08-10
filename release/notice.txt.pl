c6972b39b038652f80786763b0570ee84b79f51c
Andy Wick <andywick@gmail.com>
2017-08-10 09:45:50 -0400

    Add the capture NOTICEs at release time

--- release/notice.txt.pl
+++ release/notice.txt.pl
@@ -15,11 +15,14 @@ sub handle {
 }
 
 my %DONE;
-my $viewerJson = from_json(`cd $ARGV[1]/viewer; license-checker --production --json`);
-my $wiseServiceJson = from_json(`cd $ARGV[1]/capture/plugins/wiseService; license-checker --production --json`);
+my $viewerJson = from_json(`cd $ARGV[0]/viewer; license-checker --production --json`);
+my $wiseServiceJson = from_json(`cd $ARGV[0]/capture/plugins/wiseService; license-checker --production --json`);
 
+shift @ARGV;
 
-system("cat $ARGV[0]");
+for my $file (@ARGV) {
+    system("cat $file");
+}
 
 for my $key (keys %$viewerJson) {
     $DONE{$key} = 1;
