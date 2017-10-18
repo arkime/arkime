c1665b669be0deb4a051680491c043b2ee88a467
Andy Wick <andywick@gmail.com>
2017-10-17 20:06:17 -0400

    run tests without sandbox for now

--- viewer/karma.conf.js
+++ viewer/karma.conf.js
@@ -69,7 +69,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeNoSandbox'],
+
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
