be0cd146568d22fdadccc960036ae61d430e9e43
Aaron Hodgen <ahodgen@kill-9.org>
2017-11-21 10:02:55 -0500

    Optionally index HTTP request bodies (#772)
    * Optionally index HTTP request bodies
    
    * Remove redundant option. Correct field name.

--- capture/moloch.h
+++ capture/moloch.h
@@ -347,6 +347,7 @@ typedef struct moloch_config {
     uint32_t  maxFrags;
     uint32_t  snapLen;
     uint32_t  maxMemPercentage;
+    uint32_t  maxReqBody;
 
     int       packetThreads;
 
@@ -357,6 +358,7 @@ typedef struct moloch_config {
     char      parseSMB;
     char      parseQSValue;
     char      parseCookieValue;
+    char      reqBodyOnlyUtf8;
     char      compressES;
     char      antiSynDrop;
     char      readTruncatedPackets;
