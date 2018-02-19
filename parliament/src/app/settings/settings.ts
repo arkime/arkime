c530e669db6bb9b95137bff1af7990492ba5e145
31453 <elyse.rinne@teamaol.com>
2018-02-19 10:04:03 -0500

    added Alert and Notifier models

--- parliament/src/app/settings/settings.ts
+++ parliament/src/app/settings/settings.ts
@@ -1,5 +1,15 @@
+export class Alert {
+  id: string;
+  on: boolean;
+  name: string;
+  description: string;
+}
+
 export class Notifier {
+  name: string;
+  on: boolean;
   fields: [{ name: string }];
+  alerts: [Alert];
 }
 
 export class Settings {
