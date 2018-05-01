3ee75e1b7c8dec6e7fe881d15fe3ff081363075e
Andy Wick <andywick@gmail.com>
2018-05-01 13:48:12 -0400

    Reindex stuff to prepare for ES 6

--- viewer/multies.js
+++ viewer/multies.js
@@ -130,7 +130,7 @@ function simpleGather(req, res, bodies, doneCb) {
       pres.on('end', () => {
         if (result.length) {
           result = result.replace(new RegExp('(index":\s*|[,{]|  )"' + prefix + "(sessions2|sessions|stats|dstats|sequence|files|users|history)", "g"), "$1\"MULTIPREFIX_$2");
-          result = result.replace(new RegExp('(index":\s*)"' + prefix + "(fields_v1)\"", "g"), "$1\"MULTIPREFIX_$2\"");
+          result = result.replace(new RegExp('(index":\s*)"' + prefix + "(fields_v[12])\"", "g"), "$1\"MULTIPREFIX_$2\"");
           result = JSON.parse(result);
         } else {
           result = {};
