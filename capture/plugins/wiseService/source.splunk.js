fc6fcda2519746d192a842a44a73e5a3866d2070
Andy Wick <andywick@gmail.com>
2018-06-07 12:29:10 -0400

    initial splunk source

---
+++ capture/plugins/wiseService/source.splunk.js
@@ -0,0 +1,221 @@
+/******************************************************************************/
+/*
+ *
+ * Copyright 2012-2016 AOL Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this Software except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+var wiseSource     = require('./wiseSource.js')
+  , util           = require('util')
+  , splunkjs       = require('splunk-sdk')
+  , iptrie         = require('iptrie')
+  , HashTable      = require('hashtable')
+  ;
+
+//////////////////////////////////////////////////////////////////////////////////
+function SplunkSource (api, section) {
+  SplunkSource.super_.call(this, api, section);
+
+  this.host             = api.getConfig(section, "host");
+  this.username         = api.getConfig(section, "username");
+  this.password         = api.getConfig(section, "password");
+  this.version          = api.getConfig(section, "version", 5);
+  this.port             = api.getConfig(section, "port", 8089);
+  this.periodic         = api.getConfig(section, "periodic");
+  this.query            = api.getConfig(section, "query");
+  this.keyColumn        = api.getConfig(section, "keyColumn");
+
+  this.typeSetting();
+  this.tagsSetting();
+
+  ["host", "username", "password", "query", "keyColumn"].forEach((item) => {
+    if (this[item] === undefined) {
+      console.log(this.section, `- ERROR not loading since no ${item} specified in config file`);
+      return;
+    }
+  });
+
+  if (this.periodic) {
+    this.cacheTimeout = -1; // Don't cache
+    switch (this.type) {
+    case "domain":
+      this.getDomain = this.sendResultPeriodic;
+      break;
+    case "ip":
+      this.getIp = this.sendResultPeriodic;
+      break;
+    case "md5":
+      this.getMd5 = this.sendResultPeriodic;
+      break;
+    case "email":
+      this.getEmail = this.sendResultPeriodic;
+      break;
+    case "url":
+      this.getURL = this.sendResultPeriodic;
+      break;
+    default:
+      console.log(this.section, "- ERROR not loading since unknown type specified in config file", this.type);
+      return false;
+    }
+    setInterval(this.periodicRefresh.bind(this), 1000 * this.periodic);
+  } else {
+    switch (this.type) {
+    case "domain":
+      this.getDomain = this.sendResult;
+      break;
+    case "ip":
+      this.getIp = this.sendResult;
+      break;
+    case "md5":
+      this.getMd5 = this.sendResult;
+      break;
+    case "email":
+      this.getEmail = this.sendResult;
+      break;
+    case "url":
+      this.getURL = this.sendResult;
+      break;
+    default:
+      console.log(this.section, "- ERROR not loading since unknown type specified in config file", this.type);
+      return false;
+    }
+  }
+
+  this.service = new splunkjs.Service({username: this.username, password: this.password, host: this.host, port: this.port, version: this.version});
+
+  this.service.login((err, success) => {
+    if (err) {
+      throw err;
+    }
+    if (this.periodic) {
+      this.periodicRefresh();
+    }
+    
+    console.log("Login was successful: " + success);
+  });
+
+  api.addSource(section, this);
+
+  this.sourceFields = [this.esResultField];
+  for (var k in this.shortcuts) {
+    if (this.sourceFields.indexOf(k) === -1)
+      this.sourceFields.push(k);
+  }
+}
+util.inherits(SplunkSource, wiseSource);
+
+//////////////////////////////////////////////////////////////////////////////////
+SplunkSource.prototype.periodicRefresh = function() {
+  this.service.oneshotSearch(this.query, {output_mode: "json", count: 0}, (err, results) => {
+    if (err) {
+      console.log(this.section, "- ERROR", err);
+      return;
+    }
+
+    var cache;
+    if (this.type === "ip") {
+      cache = new iptrie.IPTrie();
+    } else {
+      cache = new HashTable();
+    }
+
+    for (let item of results.results) {
+      var key = item[this.keyColumn];
+      if (!key) {continue;}
+
+      var args = [];
+      for (var k in this.shortcuts) {
+        if (item[k] !== undefined) {
+          args.push(this.shortcuts[k]);
+          if (Array.isArray(item[k]))
+            args.push(item[k][0]);
+          else
+            args.push(item[k]);
+        }
+      }
+
+      var newitem = {num: args.length/2, buffer: wiseSource.encode.apply(null, args)};
+
+      if (this.type === "ip") {
+        var parts = key.split("/");
+        cache.add(parts[0], +parts[1] || (parts[0].includes(':')?128:32), newitem);
+      } else {
+        cache.put(key, newitem);
+      }
+    }
+    this.cache = cache;
+  });
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+SplunkSource.prototype.sendResultPeriodic = function(key, cb) {
+  if (!this.cache) {
+    return cb(null, undefined);
+  }
+
+  var result = this.type === "ip"?this.cache.find(key):this.cache.get(key);
+
+  // Not found, or found but no extra values to add
+  if (!result) {
+    return cb(null, undefined);
+  }
+  if (result.num === 0) {
+    return cb(null, this.tagsResult);
+  }
+
+  // Found, so combine the two results (per item, and per source)
+  var newresult = {num: result.num + this.tagsResult.num, buffer: Buffer.concat([result.buffer, this.tagsResult.buffer])};
+  return cb(null, newresult);
+};
+
+//////////////////////////////////////////////////////////////////////////////////
+SplunkSource.prototype.sendResult = function(key, cb) {
+  var query = this.query.replace("%%SEARCHTERM%%", key);
+
+  this.service.oneshotSearch(query, {output_mode: "json", count: 0}, (err, results) => {
+    if (err) {
+      console.log(this.section, "- ERROR", err);
+      return cb(null, undefined);
+    }
+    
+    if (!results.results || results.results.length === 0) {
+      return cb(null, undefined);
+    }
+
+    var item = results.results[0];
+    var key = item[this.keyColumn];
+
+    var args = [];
+    for (var k in this.shortcuts) {
+      if (item[k] !== undefined) {
+        args.push(this.shortcuts[k]);
+        if (Array.isArray(item[k]))
+          args.push(item[k][0]);
+        else
+          args.push(item[k]);
+      }
+    }
+    var newresult = {num: args.length/2 + this.tagsResult.num, buffer: Buffer.concat([wiseSource.encode.apply(null, args), this.tagsResult.buffer])};
+    return cb(null, newresult);
+  });
+};
+//////////////////////////////////////////////////////////////////////////////////
+exports.initSource = function(api) {
+  var sections = api.getConfigSections().filter((e) => {return e.match(/^splunk:/);});
+  sections.forEach((section) => {
+    var source = new SplunkSource(api, section);
+  });
+};
+
