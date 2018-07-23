007835099e3e44790e760dff575f7c8d4ffe0c6a
Andy Wick <andywick@gmail.com>
2018-07-23 15:36:50 -0400

    try ubuntu 18

--- release/makeDockerBuild.sh
+++ release/makeDockerBuild.sh
@@ -6,7 +6,7 @@ docker image build build7 --tag andywick/moloch-build-7:$VER
 docker image build build6 --tag andywick/moloch-build-6:$VER
 docker image build build14 --tag andywick/moloch-build-14:$VER
 docker image build build16 --tag andywick/moloch-build-16:$VER
-#docker image build build18 --tag andywick/moloch-build-18:$VER
+docker image build build18 --tag andywick/moloch-build-18:$VER
 
 exit 0
 
