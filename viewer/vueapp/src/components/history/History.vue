e105deae4784764772c84d8376c049bd5eaa76e7
31453 <elyse.rinne@teamaol.com>
2018-09-17 17:08:15 -0400

    history ui improvements
    show loading indication
    make sure every key-value pair has a value
    sort by timestamp descending
    make sure values in columns are truncated with ellipses

--- viewer/vueapp/src/components/history/History.vue
+++ viewer/vueapp/src/components/history/History.vue
@@ -119,7 +119,7 @@
         <template v-for="(item, index) of history.data">
           <!-- history item -->
           <tr :key="item.id">
-            <td class="nowrap">
+            <td class="no-wrap">
               <toggle-btn class="mt-1"
                 :opened="item.expanded"
                 @toggle="toggleLogDetail(item)">
@@ -204,7 +204,7 @@
                   <span v-for="(value, key) in item.body"
                     :key="key">
                     <dt>{{ key }}</dt>
-                    <dd>{{ value }}</dd>
+                    <dd>{{ value }}&nbsp;</dd>
                   </span>
                 </div> <!-- /req body -->
                 <!-- query params -->
@@ -222,7 +222,7 @@
                     :key="key">
                     <dt>{{ key }}</dt>
                     <dd>
-                      {{ value }}
+                      {{ value }}&nbsp;
                       <span v-if="key === 'view' && item.view && item.view.expression">
                         ({{ item.view.expression }})
                       </span>
@@ -254,6 +254,11 @@
         </template>
       </tbody>
     </table>
+
+    <!-- loading overlay -->
+    <moloch-loading
+      v-if="loading && !error">
+    </moloch-loading> <!-- /loading overlay -->
 
     <!-- hack to make vue watch expanded logs -->
     <div style="display:none;">
@@ -293,8 +298,8 @@ export default {
       filters: {},
       columns: [
         { name: 'Time', sort: 'timestamp', nowrap: true, width: 10, help: 'The time of the request' },
-        { name: 'Time Range', sort: 'range', nowrap: true, width: 8, classes: 'text-right', help: 'The time range of the request' },
-        { name: 'User ID', sort: 'userId', nowrap: true, width: 11, filter: true, permission: 'createEnabled', help: 'The id of the user that initiated the request' },
+        { name: 'Time Range', sort: 'range', nowrap: true, width: 11, classes: 'text-right', help: 'The time range of the request' },
+        { name: 'User ID', sort: 'userId', nowrap: true, width: 8, filter: true, permission: 'createEnabled', help: 'The id of the user that initiated the request' },
         { name: 'Query Time', sort: 'queryTime', nowrap: true, width: 8, classes: 'text-right', help: 'Execution time in MS' },
         { name: 'API', sort: 'api', nowrap: true, width: 13, filter: true, help: 'The API endpoint of the request' },
         { name: 'Expression', sort: 'expression', nowrap: true, width: 27, exists: false, help: 'The query expression issued with the request' },
@@ -309,7 +314,7 @@ export default {
         start: 0,
         searchTerm: null,
         sortField: 'timestamp',
-        desc: false,
+        desc: true,
         date: this.$store.state.timeRange,
         startTime: this.$store.state.time.startTime,
         stopTime: this.$store.state.time.stopTime
@@ -460,7 +465,7 @@ export default {
   margin-top: 160px;
   table-layout: fixed;
 }
-.history-page table tbody tr td.nowrap {
+.history-page table tbody tr td.no-wrap {
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
