974e3ef4dd352645877fcad9c9149aa778be23dc
Phil Hagen <phil@lewestech.com>
2018-11-20 17:45:48 -0500

    exclude data/log dirs from tracking in packages, creating in post-install instead.  fixes #963

--- release/afterinstall.sh
+++ release/afterinstall.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
+
+# re-create these directories after installation so they are not part of the package manifest
+CREATEDIRS="/data/<%= name %>/logs /data/<%= name %>/raw"
+for CREATEDIR in ${CREATEDIRS}; do
+    if [ ! -e ${CREATEDIR} ]; then
+        mkdir -m 0700 -p ${CREATEDIR} && \
+        chown nobody ${CREATEDIR}
+    fi
+done
+
 echo "READ /data/<%= name %>/README.txt and RUN /data/<%= name %>/bin/Configure"
