28d75bf8391b487d192d4fb3be08abfbd2eeb781
Andy Wick <awick@purple.org>
2019-01-17 17:51:19 -0500

    release building (#996)
    Add release building using screwdriver, hopefully

--- release/notice.txt.pl
+++ release/notice.txt.pl
@@ -15,13 +15,25 @@ sub handle {
 }
 
 my %DONE;
+my $topJson = from_json(`license-checker --production --json`);
+my $parliamentJson = from_json(`cd $ARGV[0]/parliament; license-checker --production --json`);
 my $viewerJson = from_json(`cd $ARGV[0]/viewer; license-checker --production --json`);
-my $wiseServiceJson = from_json(`cd $ARGV[0]/capture/plugins/wiseService; license-checker --production --json`);
+my $wiseServiceJson = from_json(`cd $ARGV[0]/wiseService; license-checker --production --json`);
 
 shift @ARGV;
 
 for my $file (@ARGV) {
     system("cat $file");
+}
+
+for my $key (keys %$topJson) {
+    $DONE{$key} = 1;
+    handle($key, $topJson->{$key});
+}
+
+for my $key (keys %$parliamentJson) {
+    $DONE{$key} = 1;
+    handle($key, $parliamentJson->{$key});
 }
 
 for my $key (keys %$viewerJson) {
