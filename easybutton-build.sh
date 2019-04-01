b7a584a2f88992ae4a074194830148bd55686b68
Andy Wick <andywick@gmail.com>
2019-04-01 15:16:47 -0400

    upgrade versions

--- easybutton-build.sh
+++ easybutton-build.sh
@@ -11,14 +11,14 @@
 # * install moloch if --install
 
 
-GLIB=2.54.3
+GLIB=2.56.2
 YARA=3.7.1
 MAXMIND=1.3.2
-PCAP=1.8.1
-CURL=7.59.0
-LUA=5.3.4
+PCAP=1.9.0
+CURL=7.61.1
+LUA=5.3.5
 DAQ=2.0.6
-NODE=8.11.1
+NODE=8.15.0
 
 TDIR="/data/moloch"
 DOPFRING=0
