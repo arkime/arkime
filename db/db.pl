00b003053bdccee5d86d768e4faf4483a13986d1
Scott Schenkein <schenksj@yahoo.com>
2019-06-20 20:29:45 -0400

    fix merge errors

--- db/db.pl
+++ db/db.pl
@@ -56,6 +56,7 @@
 # 59 - tokens
 # 60 - users time query limit
 # 61 - shortcuts
+# 62 - hunt error timestamp and node
 
 use HTTP::Request::Common;
 use LWP::UserAgent;
@@ -64,7 +65,7 @@ use Data::Dumper;
 use POSIX;
 use strict;
 
-my $VERSION = 61;
+my $VERSION = 62;
 my $verbose = 0;
 my $PREFIX = "";
 my $SECURE = 1;
@@ -1513,6 +1514,12 @@ sub huntsUpdate
         "properties": {
           "value": {
             "type": "keyword"
+          },
+          "time": {
+            "type": "date"
+          },
+          "node": {
+            "type": "keyword"
           }
         }
       },
@@ -2108,11 +2115,6 @@ if ($CLIENTCERT ne "") {
         SSL_verify_mode => $SECURE,
         verify_hostname=> $SECURE
     )
-}
-
-if ($CLIENTKEY != "") {
-   $ENV{HTTPS_CERT_FILE} = $CLIENTCERT;
-   $ENV{HTTPS_KEY_FILE} = $CLIENTKEY
 }
 
 if ($ARGV[0] =~ /^http/) {
@@ -3024,6 +3026,7 @@ if ($ARGV[1] =~ /^(init|wipe|clean)/) {
         setPriority();
         queriesUpdate();
         sessions2Update();
+        huntsUpdate();
         fieldsIpDst();
     } elsif ($main::versionNumber <= 58) {
         checkForOld5Indices();
@@ -3039,9 +3042,11 @@ if ($ARGV[1] =~ /^(init|wipe|clean)/) {
         sessions2Update();
         usersUpdate();
         lookupsCreate();
-    } elsif ($main::versionNumber <= 61) {
+        huntsUpdate();
+    } elsif ($main::versionNumber <= 62) {
         checkForOld5Indices();
         sessions2Update();
+        huntsUpdate();
     } else {
         logmsg "db.pl is hosed\n";
     }
