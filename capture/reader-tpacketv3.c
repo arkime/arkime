d1a818cd7bb245d4ed9668d6edf6d54d7bb6558b
Scott Schenkein <schenksj@yahoo.com>
2019-10-24 16:49:18 -0400

    Unique fanout_group_id with multiple NICS (#1218)

--- capture/reader-tpacketv3.c
+++ capture/reader-tpacketv3.c
@@ -276,7 +276,7 @@ void reader_tpacketv3_init(char *UNUSED(name))
         
         if (fanout_group_id != 0) {
             int fanout_type = PACKET_FANOUT_HASH;
-            int fanout_arg = (fanout_group_id | (fanout_type << 16));
+            int fanout_arg = ((fanout_group_id+i) | (fanout_type << 16));
             if(setsockopt(infos[i].fd, SOL_PACKET, PACKET_FANOUT, &fanout_arg, sizeof(fanout_arg)) < 0)
                 LOGEXIT("Error setting packet fanout parameters: (%d,%s)", fanout_group_id, strerror(errno));
         }
