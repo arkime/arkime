c5c9de1651c71c9e9475db746e99ad6e368f3655
Mala Famero <none_of_the_above@wearehackerone.com>
2019-10-28 21:02:23 +0000

    Report 724192 from HackerOne (Mala Fama team)

--- parliament/parliament.js
+++ parliament/parliament.js
@@ -1470,6 +1470,14 @@ router.post('/groups/:id/clusters', verifyToken, (req, res, next) => {
     return next(error);
   }
 
+  if (!(req.body.url.startsWith('https://') || req.body.url.startsWith('http://'))) {
+    let message;
+    message = 'Invalid URL. The cluster URL must start with "http" or "https"';
+    const error = new Error(message);
+    error.httpStatusCode = 422;
+    return next(error);
+  }
+
   let newCluster = {
     title       : req.body.title,
     description : req.body.description,
