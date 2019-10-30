786b9713547f8d8e800305d03201b8fcd69dead0
Mahmud Hossain <mahmud.csebuet@gmail.com>
2019-10-30 08:19:41 -0400

    Maintain a list of active ES cluster (#1235)
    * maintain a list of active or alive ES cluster
    
    * fix for npm lint
    
    * added a comment
    
    * code refactoring

--- viewer/multies.js
+++ viewer/multies.js
@@ -117,7 +117,14 @@ function node2Prefix(node) {
   return "";
 }
 
+var activeESNodes = [];
+
+function getActiveNodes(){
+  return activeESNodes.slice();
+}
+
 function simpleGather(req, res, bodies, doneCb) {
+  var nodes = getActiveNodes();
   async.map(nodes, (node, asyncCb) => {
     var result = "";
     var url = node2Url(node) + req.url;
@@ -633,7 +640,7 @@ app.post("/:index/:type/_search", function(req, res) {
   var bodies = {};
   var search = JSON.parse(req.body);
   //console.log("DEBUG - INCOMING SEARCH", JSON.stringify(search, null, 2));
-
+  var nodes = getActiveNodes();
   async.each(nodes, (node, asyncCb) => {
     fixQuery(node, req.body, (err, body) => {
       //console.log("DEBUG - OUTGOING SEARCH", node, JSON.stringify(body, null, 2));
@@ -666,7 +673,7 @@ app.post("/:index/:type/_search", function(req, res) {
 function msearch(req, res) {
   var lines = req.body.split(/[\r\n]/);
   var bodies = {};
-
+  var nodes = getActiveNodes();
   async.each(nodes, (node, nodeCb) => {
     var nlines = [];
     async.eachSeries(lines, (line, lineCb) => {
@@ -784,6 +791,43 @@ nodes.forEach((node) => {
     }
   });
 });
+
+activeESNodes = nodes.slice();
+
+// Ping (HEAD /) periodically to maintian a list of active ES nodes
+function pingESNode(client, node){
+  return new Promise((resolve, reject) => {
+      client.ping({
+        requestTimeout: 3*1000 //ping usually has a 3000ms timeout
+      }, function (error) {
+        resolve({isActive: error ? false : true, node: node});
+      });
+    });
+}
+
+function enumerateActiveNodes(){
+  var ping_tasks = [];
+  for (var i = 0; i < nodes.length; i++) {
+    ping_tasks.push(pingESNode(clients[nodes[i]], nodes[i]));
+  }
+  Promise.all(ping_tasks).then(function(values) {
+    var active_nodes = [];
+    for (var i = 0; i < values.length; i++) {
+      if(values[i].isActive) { // true -> node is active
+        active_nodes.push(values[i].node);
+      } else { // false -> node is down
+        // remove credential from the url
+        var host = values[i].node.split("://");
+        host = host[host.length > 1 ? 1 : 0 ].split("@"); // user:pass@elastic.com:9200
+        host = host [host.length > 1 ? host.length-1 : 0];
+        console.log("Elasticsearch is down at ", host);
+      }
+    }
+    activeESNodes = active_nodes.slice();
+  });
+}
+
+setInterval(enumerateActiveNodes, 1*60*1000); // 1*60*1000 ms
 
 console.log(nodes);
 
