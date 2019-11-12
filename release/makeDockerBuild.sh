deb5c57db38bf3b6576cc5fc60259d68570f60a9
Markus Vogl <vogl91@gmail.com>
2019-11-12 19:15:19 +0100

    Actual docker build (without installing) + push to dockerhub (feel free to remove this)

--- release/makeDockerBuild.sh
+++ release/makeDockerBuild.sh
@@ -7,6 +7,7 @@ docker image build build6 --tag andywick/moloch-build-6:$VER
 docker image build build14 --tag andywick/moloch-build-14:$VER
 docker image build build16 --tag andywick/moloch-build-16:$VER
 docker image build build18 --tag andywick/moloch-build-18:$VER
+docker image build build19--tag andywick/moloch-build-19:$VER
 
 exit 0
 
@@ -15,3 +16,4 @@ docker push andywick/moloch-build-6:$VER
 docker push andywick/moloch-build-14:$VER
 docker push andywick/moloch-build-16:$VER
 docker push andywick/moloch-build-18:$VER
+docker push andywick/moloch-build-19:$VER
