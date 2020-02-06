f8990b75f0221cdec793bf2b535b9ce714bc2f1d
Philip Gladstone <pjsg@users.noreply.github.com>
2020-02-06 08:07:30 -0500

    Allow more control over the logging in viewer (#1376)
    * Allow more control over the logging
    
    * Fix lint
    
    * Add the array for the list of paths

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -270,8 +270,12 @@ if (_accesslogfile) {
   _stream = fs.createWriteStream(_accesslogfile, {flags: 'a'});
 }
 
+var _logger_format = decodeURIComponent(Config.get("accessLogFormat",
+       ':date :username %1b[1m:method%1b[0m %1b[33m:url%1b[0m :status :res[content-length] bytes :response-time ms'));
+var _suppressPaths = Config.getArray("accessLogSuppressPaths", ";", "");
 
-app.use(logger(':date :username \x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :status :res[content-length] bytes :response-time ms',{stream: _stream}));
+app.use(logger(_logger_format, {stream: _stream,
+  skip: (req, res) => { return _suppressPaths.includes(req.path); }}));
 app.use(compression());
 app.use(methodOverride());
 
