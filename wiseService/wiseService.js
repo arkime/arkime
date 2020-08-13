e692552f17ae3c2fe4c1917ce56d75da3c3922c1
Andy Wick <andywick@gmail.com>
2020-08-13 16:08:06 -0400

    revert fromWildExp

--- wiseService/wiseService.js
+++ wiseService/wiseService.js
@@ -930,12 +930,8 @@ app.use((req, res, next) => {
  Revised: April 6, 2011
 */
 /* eslint-disable */
-RegExp.fromWildExp = function (c, a) {
- for (var d = a && a.indexOf('o') > -1, f, b, e = '', g = a && a.indexOf('l') > -1 ? '' : '?', h = RegExp('~.|\\[!|' + (d ? '{\\d+,?\\d*\\}|[' : '[') + (a && a.indexOf('p') > -1 ? '' : '\\(\\)') + '\\{\\}\\\\\\.\\*\\+\\?\\:\\|\\^\\$%_#<>]'); (f = c.search(h)) > -1 && f < c.length;) {
- e += c.substring(0, f), e += (b = c.match(h)[0]) == '[!' ? '[^' : b.charAt(0) == '~' ? '\\' + b.charAt(1) : b == '*' || b == '%' ? '.*' + g
-: b == '?' || b == '_' ? '.' : b == '#' ? '\\d' : d && b.charAt(0) == '{' ? b + g : b == '<' ? '\\b(?=\\w)' : b == '>' ? '(?:\\b$|(?=\\W)\\b)' : '\\' + b, c = c.substring(f + b.length);
-}e += c; a && (/[ab]/.test(a) && (e = '^' + e), /[ae]/.test(a) && (e += '$')); return RegExp(e, a ? a.replace(/[^gim]/g, '') : '');
-};
+RegExp.fromWildExp=function(c,a){for(var d=a&&a.indexOf("o")>-1,f,b,e="",g=a&&a.indexOf("l")>-1?"":"?",h=RegExp("~.|\\[!|"+(d?"{\\d+,?\\d*\\}|[":"[")+(a&&a.indexOf("p")>-1?"":"\\(\\)")+"\\{\\}\\\\\\.\\*\\+\\?\\:\\|\\^\\$%_#<>]");(f=c.search(h))>-1&&f<c.length;)e+=c.substring(0,f),e+=(b=c.match(h)[0])=="[!"?"[^":b.charAt(0)=="~"?"\\"+b.charAt(1):b=="*"||b=="%"?".*"+g:
+b=="?"||b=="_"?".":b=="#"?"\\d":d&&b.charAt(0)=="{"?b+g:b=="<"?"\\b(?=\\w)":b==">"?"(?:\\b$|(?=\\W)\\b)":"\\"+b,c=c.substring(f+b.length);e+=c;a&&(/[ab]/.test(a)&&(e="^"+e),/[ae]/.test(a)&&(e+="$"));return RegExp(e,a?a.replace(/[^gim]/g,""):"")};
 /* eslint-enable */
 // ----------------------------------------------------------------------------
 /// / Main
