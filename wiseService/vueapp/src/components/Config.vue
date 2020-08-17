fb56e390a5a62227344ee6aca1c5a9afce9b806c
Bryce Vonilten <bryce.vonilten@verizonmedia.com>
2020-08-17 13:58:59 -0700

    Edit config options page init

--- wiseService/vueapp/src/components/Config.vue
+++ wiseService/vueapp/src/components/Config.vue
@@ -1,11 +1,214 @@
 <template>
-  <div class="create-ini">
-    <h1>Create ini page</h1>
+  <!-- container -->
+  <div>
+    <Error :initialError="error" v-on:clear-initialError="error = ''"/>
+
+    <b-button
+      class="ml-auto mr-2"
+      style="display: block"
+      variant="primary"
+      disabled
+      @click="saveConfig()"
+    >
+      Save Config
+    </b-button>
+
+    <div class="d-flex flex-row">
+      <!-- Sources sidebar -->
+      <div class="d-flex flex-column">
+        <div
+          v-for="sourceKey in sidebarOptions"
+          :key="sourceKey + '-tab'"
+        >
+          <button
+            @click="selectedSourceKey = sourceKey"
+            type="button"
+            class="btn btn-light source-btn btn-outline-dark"
+          >
+            <h5>
+              {{ sourceKey }}
+            </h5>
+          </button>
+
+          <hr class="mx-3" v-if="sourceKey === 'wiseService'"/>
+        </div>
+
+        <span class="px-3">
+          <hr/>
+          <b-button variant="success" class="text-nowrap" @click="showSourceModal = true">
+            <b-icon icon="plus" scale="1"></b-icon>
+            <span>Add Source</span>
+          </b-button>
+        </span>
+      </div> <!-- /Sources sidebar -->
+
+      <!-- Selected Source Inputs -->
+      <div class="d-flex flex-column px-5 pt-2 w-100">
+        <h2 class="text-center">{{selectedSourceKey}}</h2>
+
+        <div class="d-flex flex-row" v-if="configDefs && configDefs[selectedSourceSplit] && configDefs[selectedSourceSplit].fields">
+          <div class="d-flex flex-column">
+            <div
+              v-for="field in configDefs[selectedSourceSplit].fields"
+              :key="field.name + '-label'"
+              class="d-flex flex-column text-nowrap py-2 align-items-left mr-2 input-label"
+            >
+              {{ field.name }} :
+            </div>
+          </div>
+
+          <div class="d-flex flex-column w-100">
+            <div
+              v-for="field in configDefs[selectedSourceSplit].fields"
+              :key="field.name + '-input'"
+              class="d-flex flex-column py-2"
+            >
+              <b-form-input
+                v-if="currConfig && currConfig[selectedSourceKey]"
+                class="input-box"
+                v-model="currConfig[selectedSourceKey][field.name]"
+                :placeholder="field.help"
+                :required="field.required"
+                v-b-popover.focus.top="field.help"
+              >
+              </b-form-input>
+            </div>
+          </div>
+        </div>
+
+        <b-button v-if="selectedSourceKey !== 'wiseService'" variant="danger" class="mx-auto mt-4" style="display:block" @click="deleteSource()">
+          <b-icon icon="trash" scale="1"></b-icon>
+          Delete Source
+        </b-button>
+      </div><!-- /Selected Source Inputs -->
+    </div>
+
+    <b-modal
+      v-model="showSourceModal"
+      title="New Source"
+    >
+      <b-container fluid>
+        <!-- <select
+          <option v-for="source in Object.keys(configDefs.sources)" :value="type" :key="source + 'Option'">
+            {{ type }}
+          </option>
+        </select> -->
+      </b-container>
+
+      <template v-slot:modal-footer>
+        <div class="w-100">
+          <b-button
+            variant="primary"
+            size="sm"
+            class="float-right"
+            @click="showSourceModal = false"
+          >
+            Close
+          </b-button>
+          <b-button
+            variant="success"
+            size="sm"
+            class="float-right mr-2"
+            @click=""
+          >
+            Create
+          </b-button>
+        </div>
+      </template>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import WiseService from './wise.service';
+import Error from './Error';
+
 export default {
-  name: 'Config'
+  name: 'Config',
+  components: {
+    Error
+  },
+  mounted: function () {
+    this.loadConfigDefs();
+    this.loadCurrConfig();
+  },
+  data: function () {
+    return {
+      error: '',
+      showSourceModal: false,
+      selectedSourceKey: 'wiseService',
+      configDefs: {},
+      currConfig: {}
+    };
+  },
+  computed: {
+    selectedSourceSplit: function () {
+      return this.selectedSourceKey.split(':')[0];
+    },
+    sidebarOptions: function () {
+      // Sort wiseService to first option in sidebar
+      let opts = Object.keys(this.currConfig);
+      let wiseIndex = opts.indexOf('wiseService');
+
+      if (wiseIndex >= 0) {
+        opts.splice(wiseIndex, 1);
+      }
+
+      opts.unshift('wiseService');
+      return opts;
+    }
+  },
+  methods: {
+    deleteSource: function () {
+      delete this.currConfig[this.selectedSourceKey];
+      this.selectedSourceKey = '';
+    },
+    saveConfig: function () {
+      // TODO
+    },
+    loadConfigDefs: function () {
+      WiseService.getConfigDefs()
+        .then((data) => {
+          this.error = '';
+          this.configDefs = data;
+        })
+        .catch((error) => {
+          this.error = error.text ||
+            `Error fetching config definitions from wise.`;
+        });
+    },
+    loadCurrConfig: function () {
+      WiseService.getCurrConfig()
+        .then((data) => {
+          this.error = '';
+
+          // Add wiseService regardless if its in the ini.
+          if (!data.hasOwnProperty('wiseService')) {
+            data['wiseService'] = {}
+          }
+
+          this.currConfig = data;
+        })
+        .catch((error) => {
+          this.error = error.text ||
+            `Error fetching current config for wise.`;
+        });
+    }
+  }
 };
 </script>
+
+<style scoped>
+/* TODO: verify all the css works for dark mode */
+.source-btn {
+  width: 100%;
+  border-radius: 0;
+  background: white;
+  border-color: white;
+  box-shadow: none !important; /* Covers all css states that has this defined in boostrap */
+}
+.input-label {
+  height: 54px;
+  justify-content: center;
+}
+</style>
