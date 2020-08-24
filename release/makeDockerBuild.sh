31c648a4be1b4aad31df17550f11afb58a5e8bc8
Andy Wick <andywick@gmail.com>
2020-08-24 12:48:10 -0400

    rebuild the build images

--- release/makeDockerBuild.sh
+++ release/makeDockerBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export VER=2.4.0-2
+export VER=2.4.0-3
 #docker images -a | grep "moloch-build" | awk '{print $3}' | xargs docker rmi
 
 docker image build build8 --tag andywick/moloch-build-8:$VER
