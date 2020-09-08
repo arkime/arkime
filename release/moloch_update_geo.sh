44b84400bf617125ef7f97aedc24a44635ab6d77
sickcodes <info@sick.codes>
2020-09-08 20:12:35 +0000

    Remove useless use of dot, e.g. in case of MOLOCH_DIR=. resulting in ..

--- release/moloch_update_geo.sh
+++ release/moloch_update_geo.sh
@@ -6,21 +6,21 @@ TIMEOUT="${WGET_TIMEOUT:-30}"
 
 cd "${DEST_DIR}"
 
-# Remove ./ipv4-address-space.csv.tmp if it exists from a failed download, and then download the latest
-if [ -e ./ipv4-address-space.csv.tmp ]; then
-    rm -rf ./ipv4-address-space.csv.tmp
+# Remove ipv4-address-space.csv.tmp if it exists from a failed download, and then download the latest
+if [ -e ipv4-address-space.csv.tmp ]; then
+    rm -rf ipv4-address-space.csv.tmp
 fi
 
-wget -nv --timeout="${TIMEOUT}" -O ./ipv4-address-space.csv.tmp --no-check-certificate "https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv" \
-    && mv -f ./ipv4-address-space.csv.tmp ./ipv4-address-space.csv
+wget -nv --timeout="${TIMEOUT}" -O ipv4-address-space.csv.tmp --no-check-certificate "https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv" \
+    && mv -f ipv4-address-space.csv.tmp ipv4-address-space.csv
 
-# Remove ./oui.txt.tmp if it exists from a failed download, and then download the latest
-if [ -e ./oui.txt.tmp ]; then
-    rm -rf ./oui.txt.tmp
+# Remove oui.txt.tmp if it exists from a failed download, and then download the latest
+if [ -e oui.txt.tmp ]; then
+    rm -rf oui.txt.tmp
 fi
 
-wget -nv --timeout="${TIMEOUT}" -O ./oui.txt.tmp --no-check-certificate "https://raw.githubusercontent.com/wireshark/wireshark/master/manuf" \
-    && mv -f ./oui.txt.tmp ./oui.txt
+wget -nv --timeout="${TIMEOUT}" -O oui.txt.tmp --no-check-certificate "https://raw.githubusercontent.com/wireshark/wireshark/master/manuf" \
+    && mv -f oui.txt.tmp oui.txt
 
 # Call the maxind geoipupdate program if available. See
 # https://blog.maxmind.com/2019/12/18/significant-changes-to-accessing-and-using-geolite2-databases/
