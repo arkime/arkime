9ece2fab1a3a915943df5091d93495774eb0f25e
sickcodes <info@sick.codes>
2020-09-08 15:28:10 +0000

    cd "${DEST_DIR}"

--- release/moloch_update_geo.sh
+++ release/moloch_update_geo.sh
@@ -4,13 +4,15 @@
 DEST_DIR="${MOLOCH_DIR:-BUILD_MOLOCH_INSTALL_DIR}/etc"
 TIMEOUT="${WGET_TIMEOUT:-30}"
 
+cd "{$DEST_DIR}"
+
 # Remove ./ipv4-address-space.csv.tmp if it exists from a failed download, and then download the latest
 if [ -e ./ipv4-address-space.csv.tmp ]; then
     rm -rf ./ipv4-address-space.csv.tmp
 fi
 
 wget -nv --timeout="${TIMEOUT}" -O ./ipv4-address-space.csv.tmp --no-check-certificate "https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv" \
-    && mv -f ./ipv4-address-space.csv.tmp "${DEST_DIR}/ipv4-address-space.csv"
+    && mv -f ./ipv4-address-space.csv.tmp ./ipv4-address-space.csv
 
 # Remove ./oui.txt.tmp if it exists from a failed download, and then download the latest
 if [ -e ./oui.txt.tmp ]; then
@@ -18,7 +20,7 @@ if [ -e ./oui.txt.tmp ]; then
 fi
 
 wget -nv --timeout="${TIMEOUT}" -O ./oui.txt.tmp --no-check-certificate "https://raw.githubusercontent.com/wireshark/wireshark/master/manuf" \
-    && mv -f ./oui.txt.tmp "${DEST_DIR}/oui.txt"
+    && mv -f ./oui.txt.tmp ./oui.txt
 
 # Call the maxind geoipupdate program if available. See
 # https://blog.maxmind.com/2019/12/18/significant-changes-to-accessing-and-using-geolite2-databases/
