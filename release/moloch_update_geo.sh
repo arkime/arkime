ed4801db08253b9b4397ef6d98ecec8e6749aff8
Andy Wick <andywick@gmail.com>
2020-09-09 11:01:00 -0400

    Verify WGET_TIMEOUT is number, use manuf for file name

--- release/moloch_update_geo.sh
+++ release/moloch_update_geo.sh
@@ -4,23 +4,26 @@
 DEST_DIR="${MOLOCH_DIR:-BUILD_MOLOCH_INSTALL_DIR}/etc"
 TIMEOUT=${WGET_TIMEOUT:-30}
 
+# Check we have a number for timeout
+if ! [[ $TIMEOUT =~ ^[0-9]+$ ]] ; then
+    echo "WGET_TIMEOUT isn't a number '$TIMEOUT'"
+    exit 1;
+fi
+
 # Work on temp dir to not affect current working files
 cd /tmp
 
+# Try and download ipv4-address-space.csv, only copy if it works
 wget -N -nv --timeout=${TIMEOUT} --no-check-certificate https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv
-if (( $? == 0 ))
-then
+if (( $? == 0 )) ; then
   cp ipv4-address-space.csv "${DEST_DIR}"
 fi
 
-
-wget -nv --timeout=${TIMEOUT} -O oui.txt https://raw.githubusercontent.com/wireshark/wireshark/master/manuf
-if (( $? == 0 ))
-then
-  cp oui.txt "${DEST_DIR}/oui.txt"
+# Try and download manuf, only copy if it works
+wget -N -nv --timeout=${TIMEOUT} https://raw.githubusercontent.com/wireshark/wireshark/master/manuf
+if (( $? == 0 )) ; then
+  cp manuf "${DEST_DIR}/oui.txt"
 fi
-
-
 
 # Call the maxind geoipupdate program if available. See
 # https://blog.maxmind.com/2019/12/18/significant-changes-to-accessing-and-using-geolite2-databases/
