7e85b9355bcdcbd52988c96cfabb91a09b231671
Andy Wick <andywick@gmail.com>
2020-12-03 13:26:00 -0500

    for now have /logos point to assets also

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -248,6 +248,7 @@ app.use('/font-awesome', express.static(path.join(__dirname, '/../node_modules/f
 app.use('/bootstrap', express.static(path.join(__dirname, '/../node_modules/bootstrap'), { maxAge: 600 * 1000 }));
 app.use('/', express.static(path.join(__dirname, '/public'), { maxAge: 600 * 1000 }));
 app.use('/assets', express.static(path.join(__dirname, '../assets'), { maxAge: 600 * 1000 }));
+app.use('/logos', express.static(path.join(__dirname, '../assets'), { maxAge: 600 * 1000 }));
 
 // password, testing, or anonymous mode setup ---------------------------------
 if (Config.get('passwordSecret')) {
