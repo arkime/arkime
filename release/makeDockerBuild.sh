a8c56cbbb561c4b56de51c193535d2873c109fc9
Andy Wick <andywick@gmail.com>
2020-12-15 12:43:06 -0500

    fix build containers. again. maybe.

--- release/makeDockerBuild.sh
+++ release/makeDockerBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export VER=3.0.0-2
+export VER=3.0.0-3
 #docker images -a | grep "arkime-build" | awk '{print $3}' | xargs docker rmi
 
 docker image build build7 --tag andywick/arkime-build-7:$VER
