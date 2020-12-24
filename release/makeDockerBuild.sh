f3cb6d43a1850265c368e0b05901a649d70f7bf1
Andy Wick <andywick@gmail.com>
2020-12-24 08:27:42 -0500

    moar

--- release/makeDockerBuild.sh
+++ release/makeDockerBuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export VER=3.0.0-3
+export VER=3.0.0-4
 #docker images -a | grep "arkime-build" | awk '{print $3}' | xargs docker rmi
 
 docker image build build7 --tag andywick/arkime-build-7:$VER
