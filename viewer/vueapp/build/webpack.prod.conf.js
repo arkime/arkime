17a9304b082f053a748ecb1ad8a82bbc79ebc959
31453 <elyse.rinne@verizonmedia.com>
2021-02-25 17:18:01 -0500

    fix vue server renderer not finding outlet comment because it was stripped out by htmlwebpackplugin

--- viewer/vueapp/build/webpack.prod.conf.js
+++ viewer/vueapp/build/webpack.prod.conf.js
@@ -60,7 +60,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       minify: {
-        removeComments: true,
+        removeComments: false,
         collapseWhitespace: true,
         removeAttributeQuotes: true
         // more options:
