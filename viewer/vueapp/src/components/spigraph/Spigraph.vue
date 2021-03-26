130689df7194ce3f0049dd0879116998b0441785
Markus V <vogl91@gmail.com>
2021-03-26 01:15:31 +0100

    Also collapse graph

--- viewer/vueapp/src/components/spigraph/Spigraph.vue
+++ viewer/vueapp/src/components/spigraph/Spigraph.vue
@@ -146,7 +146,7 @@
     </MolochCollapsible>
 
     <!-- main visualization -->
-    <div v-if="spiGraphType === 'default' && mapData && graphData && fieldObj && capStartTimes.length">
+    <div v-if="spiGraphType === 'default' && mapData && graphData && fieldObj && capStartTimes.length && showToolBars">
       <moloch-visualizations
         id="primary"
         :graph-data="graphData"
@@ -344,6 +344,9 @@ export default {
         }
       }
       return oldFieldObj;
+    },
+    showToolBars: function () {
+      return this.$store.state.showToolBars;
     }
   },
   watch: {
