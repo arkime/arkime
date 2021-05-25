caa2adf6e80a65a97748dc623de3f48485b959f4
Andy Wick <andywick@gmail.com>
2021-05-25 17:05:51 -0400

    switch to es 7.10, stop building ubuntu 16

--- release/makeDockerBuild.sh
+++ release/makeDockerBuild.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-export VER=3.0.0-5
+export VER=3.0.0-6
 #docker images -a | grep "arkime-build" | awk '{print $3}' | xargs docker rmi
 
+echo "ARKIME DOCKER 7"
 docker image build build7 --tag andywick/arkime-build-7:$VER
+
+echo "ARKIME DOCKER 8"
 docker image build build8 --tag andywick/arkime-build-8:$VER
-docker image build build16 --tag andywick/arkime-build-16:$VER
+
+echo "ARKIME DOCKER 18"
 docker image build build18 --tag andywick/arkime-build-18:$VER
+
+echo "ARKIME DOCKER 20"
 docker image build build20 --tag andywick/arkime-build-20:$VER
+
+echo "ARKIME DOCKER Arch"
 docker image build buildArch --tag andywick/arkime-build-arch:$VER
 
 exit 0
 
 docker push andywick/arkime-build-7:$VER
 docker push andywick/arkime-build-8:$VER
-docker push andywick/arkime-build-16:$VER
 docker push andywick/arkime-build-18:$VER
 docker push andywick/arkime-build-20:$VER
 docker push andywick/arkime-build-arch:$VER
