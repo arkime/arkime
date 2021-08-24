f58ce581dc7aa75b268e6b3e4f8c7b5c8a04a685
fj604 <22716793+fj604@users.noreply.github.com>
2021-08-24 12:30:03 +0000

    fix metadata retrieval

--- capture/plugins/writer-s3.c
+++ capture/plugins/writer-s3.c
@@ -176,9 +176,12 @@ void writer_s3_part_cb (int code, unsigned char *data, int len, gpointer uw)
 unsigned char *moloch_get_instance_metadata(void *serverV, char *key, int key_len, size_t *mlen)
 {
     unsigned char *token;
-    char *requestHeaders[1];
+    char *requestHeaders[2];
     char tokenHeader[200];
-    char *tokenRequestHeaders[1] = {"X-aws-ec2-metadata-token-ttl-seconds: 30"};
+    char *tokenRequestHeaders[2];
+    tokenRequestHeaders[0] = "X-aws-ec2-metadata-token-ttl-seconds: 30";
+    tokenRequestHeaders[1] = NULL;
+    requestHeaders[1] = NULL;
     if (s3UseTokenForMetadata) {
         LOG("Requesting metadata token");
         token = moloch_http_send_sync(serverV, "PUT", "/latest/api/token", -1, NULL, 0, tokenRequestHeaders, mlen);
@@ -186,7 +189,7 @@ unsigned char *moloch_get_instance_metadata(void *serverV, char *key, int key_le
         snprintf(tokenHeader, sizeof(tokenHeader), "X-aws-ec2-metadata-token: %s", token);
         requestHeaders[0] = tokenHeader;
     } else {
-        requestHeaders [0] = NULL;
+        requestHeaders[0] = NULL;
     }
     return moloch_http_send_sync(serverV, "GET", key, key_len, NULL, 0, requestHeaders, mlen);
 }
