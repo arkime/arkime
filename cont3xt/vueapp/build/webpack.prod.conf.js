b01e2f95f19ca046f37a5f032cad14d39055774f
31453 <elyse.rinne@verizonmedia.com>
2021-10-27 17:08:11 -0400

    don't remove html comments with webpack
    (need them for vue server renderer)

--- cont3xt/vueapp/build/webpack.prod.conf.js
+++ cont3xt/vueapp/build/webpack.prod.conf.js
@@ -58,7 +58,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       template: 'index.html',
       inject: true,
       minify: {
-        removeComments: true,
+        removeComments: false,
         collapseWhitespace: true,
         removeAttributeQuotes: true
         // more options:
