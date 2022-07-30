eb1b5591e1cf21f15cd4e75e3713cb7b5e67cdd4
Andy Wick <andywick@gmail.com>
2022-07-30 16:27:42 -0400

    more docker changes

--- release/makeDockerBuild.sh
+++ release/makeDockerBuild.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-export VER=4.0.0-1
+export VER=4.0.0-4
 #docker images -a | grep "arkime-build" | awk '{print $3}' | xargs docker rmi
 
 echo "ARKIME DOCKER 7"
-docker image build build7 --tag andywick/arkime-build-7:$VER
+docker image build build7 --no-cache=true --tag andywick/arkime-build-7:$VER
 
 echo "ARKIME DOCKER 8"
-docker image build build8 --tag andywick/arkime-build-8:$VER
+docker image build build8 --no-cache=true --tag andywick/arkime-build-8:$VER
 
 echo "ARKIME DOCKER 18"
-docker image build build18 --tag andywick/arkime-build-18:$VER
+docker image build build18 --no-cache=true --tag andywick/arkime-build-18:$VER
 
 echo "ARKIME DOCKER 20"
-docker image build build20 --tag andywick/arkime-build-20:$VER
+docker image build build20 --no-cache=true --tag andywick/arkime-build-20:$VER
 
 echo "ARKIME DOCKER 22"
-docker image build build22 --tag andywick/arkime-build-22:$VER
+docker image build build22 --no-cache=true --tag andywick/arkime-build-22:$VER
 
 echo "ARKIME DOCKER Arch"
-docker image build buildArch --tag andywick/arkime-build-arch:$VER
+docker image build buildArch --no-cache=true --tag andywick/arkime-build-arch:$VER
 
 exit 0
 
