c97349961a8e8f5c99bf9889fbf32a4714d5e7c3
Toby Salusky <50775507+TobySalusky@users.noreply.github.com>
2022-08-10 15:56:24 -0400

    History shortcut and reissue (#1964)
    * history shortcut + tags and view on reissue search
    
    * temporary tags query parameter to pop new tab with the tags used during a reissued search
    
    * tags parsed before search submission
    
    * corrected query param removal
    
    * autofocus search bar on history page

--- cont3xt/vueapp/src/components/pages/Cont3xt.vue
+++ cont3xt/vueapp/src/components/pages/Cont3xt.vue
@@ -410,6 +410,7 @@ export default {
   },
   mounted () {
     // no need to parse start/stopDate query params here -- that is handled by TimeRangeInput
+    // submit, view, and tags query params are handled in watcher
 
     // needs to be unfocused to focus again later with hotkey (subsequent focuses are unfocused in store)
     this.$store.commit('SET_FOCUS_SEARCH', false);
@@ -523,9 +524,15 @@ export default {
     getFocusTagInput (val) {
       if (val) { this.$refs.tagInput.select(); }
     },
-    getImmediateSubmissionReady () { // fires once both integrations and views are loaded in from backend
-      if (!this.getImmediateSubmissionReady) {
-        return; // flag must be true!
+    // handle page-load query params -- fires once both integrations and views are loaded in from backend
+    getImmediateSubmissionReady () {
+      // only proceed when ready
+      if (!this.getImmediateSubmissionReady) { return; }
+
+      // parse tags from query parameter (used by history to reissue search with tags), then remove it
+      if (this.$route.query.tags) {
+        this.tags = this.$route.query.tags.split(',').map(tag => tag.trim());
+        this.tagDisplayCollapsed = !this.tags.length;
       }
 
       // apply 'view' query param
@@ -538,9 +545,9 @@ export default {
         this.search();
       }
 
-      // remove 'submit' query parameter
-      if (this.$route.query.submit !== undefined) {
-        this.$router.push({ query: { ...this.$route.query, submit: undefined } });
+      // remove 'submit' and 'tags' query parameters
+      if (this.$route.query.submit !== undefined || this.$route.query.tags !== undefined) {
+        this.$router.push({ query: { ...this.$route.query, submit: undefined, tags: undefined } });
       }
     }
   },
