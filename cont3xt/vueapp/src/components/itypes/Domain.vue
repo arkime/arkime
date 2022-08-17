946e4b12b30ec062c8882746da0724d2553eb85d
Toby Salusky <50775507+TobySalusky@users.noreply.github.com>
2022-08-17 14:04:36 -0400

    refactor txt groups

--- cont3xt/vueapp/src/components/itypes/Domain.vue
+++ cont3xt/vueapp/src/components/itypes/Domain.vue
@@ -87,21 +87,25 @@ export default {
     };
   },
   methods: {
-    // can split the elements in an Answer into multiple groups to have horizontal rules in between
+    // splits the elements of an Answer into groups to have horizontal rules in between
     answerGroups (recordType, answer) {
       // split txt records into domain-verification, site-verification, other
       if (recordType === 'TXT') {
-        const txtGroups = [[], [], []]; // initialize for all 3 possible groups
-        for (const txtEntry of answer) {
-          txtGroups[this.txtGroupIndex(txtEntry.data)].push(txtEntry);
-        }
-        // filter out empty groups, then sort alphabetically within groups
-        const filteredTxtGroups = txtGroups.filter(group => group.length > 0);
-        filteredTxtGroups.forEach(group => group.sort((a, b) => a.data.localeCompare(b.data)));
-        return filteredTxtGroups;
+        return this.txtGroups(answer);
       }
       // most record types only have 1 group, so make an array of size one
       return [answer];
+    },
+    // grouping for TXT records
+    txtGroups (answer) {
+      const txtGroups = [[], [], []]; // initialize for all 3 possible groups
+      for (const txtEntry of answer) {
+        txtGroups[this.txtGroupIndex(txtEntry.data)].push(txtEntry);
+      }
+      // filter out empty groups, then sort alphabetically within groups
+      const filteredTxtGroups = txtGroups.filter(group => group.length > 0);
+      filteredTxtGroups.forEach(group => group.sort((a, b) => a.data.localeCompare(b.data)));
+      return filteredTxtGroups;
     },
     txtGroupIndex (txtData) {
       if (txtData.includes('domain-verification')) { return 0; }
