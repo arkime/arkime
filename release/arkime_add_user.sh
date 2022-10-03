f012bccc5b70f52d61d59262dfec59f276f12db7
Andy Wick <andywick@amazon.com>
2022-10-03 14:32:26 -0400

    more cont3xt fixes

--- release/arkime_add_user.sh
+++ release/arkime_add_user.sh
@@ -3,7 +3,7 @@
 if [ -f "BUILD_ARKIME_INSTALL_DIR/etc/config.ini" ]; then
   BUILD_ARKIME_INSTALL_DIR/bin/node BUILD_ARKIME_INSTALL_DIR/viewer/addUser.js -c BUILD_ARKIME_INSTALL_DIR/etc/config.ini "$@"
 elif [ -f "BUILD_ARKIME_INSTALL_DIR/etc/cont3xt.ini" ]; then
-  BUILD_ARKIME_INSTALL_DIR/bin/node BUILD_ARKIME_INSTALL_DIR/viewer/addUser.js -c BUILD_ARKIME_INSTALL_DIR/etc/cont3xt.ini "$@"
+  BUILD_ARKIME_INSTALL_DIR/bin/node BUILD_ARKIME_INSTALL_DIR/viewer/addUser.js -n cont3xt -c BUILD_ARKIME_INSTALL_DIR/etc/cont3xt.ini "$@"
 else
   echo "Couldn't find config.ini or cont3xt.ini file"
 fi
