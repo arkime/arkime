39e3e627039724781142cb05ffd95252b94dbfd5
Andy Wick <awick@purple.org>
2022-10-10 12:38:52 -0400

    more changes to prepare for 4 (#2026)
    * more changes to prepare for 4
    
    * Move items from Configure to afterinstall
    
    systemd files autoinstalled as soft links
    
    * use local filename for install
    
    * correct path for systemctl
    
    * mention systemd in changelog

--- release/afterinstall.sh
+++ release/afterinstall.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-echo "READ /opt/<%= name %>/README.txt and RUN /opt/<%= name %>/bin/Configure"
+name="<%= name %>"
+################################################################################
+if [ -d "/etc/systemd" ] && [ -x "/bin/systemctl" ]; then
+    ln -sf /opt/$name/etc/arkimecapture.systemd.service /etc/systemd/system/arkimecapture.service
+    ln -sf /opt/$name/etc/arkimecont3xgt.systemd.service /etc/systemd/system/arkimecont3xgt.service
+    ln -sf /opt/$name/etc/arkimeparliament.systemd.service /etc/systemd/system/arkimeparliament.service
+    ln -sf /opt/$name/etc/arkimeviewer.systemd.service /etc/systemd/system/arkimeviewer.service
+    ln -sf /opt/$name/etc/arkimewise.systemd.service /etc/systemd/system/arkimewise.service
+    /usr/bin/systemctl daemon-reload
+    echo "Arkime systemd files linked"
+fi
+
+################################################################################
+if [ -d "/etc/logrotate.d" ] && [ ! -f "/etc/logrotate.d/$name" ]; then
+    echo "Installing logrotate /etc/logrotate.d/$name to delete files after 14 days"
+    cat << EOF > "/etc/logrotate.d/$name"
+/opt/$name/logs/capture.log
+/opt/$name/logs/viewer.log
+/opt/$name/logs/wise.log
+/opt/$name/logs/cont3xt.log
+/opt/$name/logs/parliament.log {
+    daily
+    rotate 14
+    notifempty
+    copytruncate
+}
+EOF
+else
+    echo "Not installing Arkime logrotate /etc/logrotate.d/$name"
+fi
+
+################################################################################
+echo "READ /opt/$name/README.txt and RUN /opt/$name/bin/Configure"
