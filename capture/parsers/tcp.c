742cf70419672239d163fc2da99b98034be34907
Timothy Noel <tnoelhere@gmail.com>
2024-01-11 19:44:46 -0500

    fix for issue #65, allow plugins to receive zero length TCP packets

--- capture/parsers/tcp.c
+++ capture/parsers/tcp.c
@@ -182,6 +182,9 @@ int tcp_packet_process(ArkimeSession_t *const session, ArkimePacket_t *const pac
             session->tcpSeq[packet->direction] = seq + 1;
             session->synSet |= (1 << packet->direction);
         }
+	// Send only the header up with a length 0, useful for tools like JA4Plus
+        if (pluginsCbs & ARKIME_PLUGIN_TCP)
+            arkime_plugins_cb_tcp(session, packet, 0, packet->direction);
         return 1;
     }
 
@@ -213,6 +216,9 @@ int tcp_packet_process(ArkimeSession_t *const session, ArkimePacket_t *const pac
             session->ackTime = (packet->ts.tv_sec - session->firstPacket.tv_sec) * 1000000 +
                                (packet->ts.tv_usec - session->firstPacket.tv_usec) + 1;
         }
+	// Send only the header up with a length 0, useful for tools like JA4Plus
+        if (pluginsCbs & ARKIME_PLUGIN_TCP)
+            arkime_plugins_cb_tcp(session, packet, 0, packet->direction);
     }
 
     if (tcphdr->th_flags & TH_PUSH) {
