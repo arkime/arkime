382be2219737b33ba4301444f9d3bacf0ed9470a
Toby Salusky <50775507+TobySalusky@users.noreply.github.com>
2024-08-15 10:55:40 -0400

    configure Vue 2 vs Vue 3 linting
    * updating eslint-plugin-vue caused 3 new errors in non-cont3xt
    projects, which appear worth fixing

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,20 +15,20 @@ module.exports = {
   env: {
     browser: true,
     jquery: true,
-    // jest: true
+    jest: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/vue3-essential',
+    // 'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard',
-    // 'plugin:jest/recommended'
+    'plugin:jest/recommended'
   ],
   // required to lint *.vue files
   plugins: [
     'vue',
-    // 'jest'
+    'jest'
   ],
   // add your custom rules here
   rules: {
@@ -50,8 +50,35 @@ module.exports = {
     'no-empty': 'off',
     'default-case-last': 'off',
     'no-shadow': ['error', { 'builtinGlobals': true, 'hoist': 'all', 'allow': ['err', 'req', 'res', 'stop', 'self'] }],
-    // 'jest/no-conditional-expect': 'off',
-    // 'jest/expect-expect': [ 'error', { 'assertFunctionNames': ['expect', 'getAllByText', 'getByText', 'getByPlaceholderText', 'getByTitle'] }],
+    'jest/no-conditional-expect': 'off',
+    'jest/expect-expect': [ 'error', { 'assertFunctionNames': ['expect', 'getAllByText', 'getByText', 'getByPlaceholderText', 'getByTitle'] }],
     'quotes': ['error', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
-  }
-}
+  },
+  // used to split up Vue 2 vs Vue 3 linting
+  overrides: [
+    // Vue 2 linting -----------------------
+    {
+      files: ['viewer/**/*.vue', 'parliament/**/*.vue', 'wiseService/**/*.vue', 'common/**/*.vue'],
+      extends: [
+        // consider switching to `plugin:vue/vue3-strongly-recommended` or `plugin:vue/vue3-recommended` for stricter rules.
+        'plugin:vue/essential'
+      ],
+      rules: {
+        'vue/multi-word-component-names': 'off'
+      }
+    },
+    // Vue 3 linting -----------------------
+    {
+      files: ['cont3xt/**/*.vue'],
+      extends: [
+        // consider switching to `plugin:vue/vue3-strongly-recommended` or `plugin:vue/vue3-recommended` for stricter rules.
+        'plugin:vue/vue3-essential'
+      ],
+      rules: {
+        'vue/multi-word-component-names': 'off',
+        // allow modifiers for v-slots like `#item.buttons`, since this style is used by Vuetify's slots (eg. datatables)
+        'vue/valid-v-slot': ['error', { allowModifiers: true }]
+      }
+    }
+  ]
+};
--- clear_npm.sh
+++
@@ -1,3 +0,0 @@
-find . -name 'node_modules' -type d -prune -exec rm -rf '{}' +;
-find . -name 'package-lock.json' -prune -exec rm '{}' +;
-
