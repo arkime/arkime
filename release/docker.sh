c96a8655e6870c9b103483d753509a5e8cd57cc0
Andy Wick <awick@purple.org>
2024-10-30 15:20:16 -0400

    Docker6 (#2974)
    * build docker
    
    * install docker
    
    * fix actions
    
    * try podman
    
    * my heart
    
    * try using docker actions
    
    * install docker
    
    * update before install
    
    * try pushing and setting ARKIME_VERSION
    
    * again again
    
    * tag and build are different
    
    * try quotes
    
    * change deb latest names to match official
    
    * quotes are evil, making progress
    
    * update permissions
    
    * add cont3xt
    
    * BASER to BASE
    
    * only push to docker on merge

---
+++ release/docker.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+export BASEDIR=/opt/arkime
+
+
+if [ ! -f $BASEDIR/etc/config.ini ]; then
+    echo "WARNING - Config file '$BASEDIR/etc/config.ini' not found"
+fi
+
+######################################################################
+run_viewer_forever() {
+    while true; do
+        (cd $BASEDIR/viewer; $BASEDIR/bin/node viewer.js)
+        sleep 1
+    done
+}
+
+######################################################################
+run_cont3xt_forever() {
+    $BASEDIR/bin/arkime_config_interfaces.sh
+    while true; do
+        (cd $BASEDIR/cont3xt; $BASEDIR/bin/node cont3xt.js)
+        sleep 1
+    done
+}
+
+######################################################################
+run_capture_forever() {
+    $BASEDIR/bin/arkime_config_interfaces.sh
+    while true; do
+        (cd $BASEDIR/bin; ./capture)
+        sleep 1
+    done
+}
+
+######################################################################
+# Function to kill all background processes on script exit
+cleanup() {
+    echo "Stopping all programs..."
+    pkill -P $$  # Kill all child processes of the current script
+    exit 0
+}
+# Trap SIGINT (Ctrl+C) and call the cleanup function
+trap cleanup SIGINT
+
+
+######################################################################
+# Figure out what to run
+case "$1" in
+    viewer)
+        echo "Running viewer"
+        run_viewer_forever &
+        ;;
+    cont3xt)
+        echo "Running cont3xt"
+        run_cont3xt_forever &
+        ;;
+    capture)
+        echo "capture"
+        run_capture_forever &
+        ;;
+    capture-viewer)
+        run_capture_forever &
+        run_viewer_forever &
+        ;;
+    *)
+        echo "Usage: $0 {viewer|capture|capture-viewer}"
+        exit 1
+        ;;
+esac
+
+wait
+
