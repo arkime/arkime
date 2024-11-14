4dd4254dccd053b94994b1db13bfa3f98fabd141
Andy Wick <andywick@amazon.com>
2024-11-14 15:04:56 -0500

    update dev6 version of git to support ARKIME_BUILD*

--- cont3xt/common/git.js
+++ cont3xt/common/git.js
@@ -5,6 +5,11 @@
 import { execSync } from 'child_process';
 
 export function git (command) {
-  // eslint-disable-next-line camelcase
+  if (command === 'describe --tags' && process.env.ARKIME_BUILD_FULL_VERSION) {
+    return process.env.ARKIME_BUILD_FULL_VERSION;
+  } else if (command === 'log -1 --format=%aI' && process.env.ARKIME_BUILD_DATE) {
+    return process.env.ARKIME_BUILD_DATE;
+  }
+
   return execSync(`git ${command}`, { encoding: 'utf8' }).trim();
 }
