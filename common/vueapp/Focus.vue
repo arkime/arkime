016d1a62a3dfccaca28e2c042c8ac30264b7ebb7
31453 <ecrinne@gmail.com>
2025-06-24 15:07:30 -0400

    fix common focus

--- common/vueapp/Focus.vue
+++ common/vueapp/Focus.vue
@@ -5,10 +5,10 @@ SPDX-License-Identifier: Apache-2.0
 <script>
 export default {
   name: 'focus',
-  mounted: function (el, binding, vnode) {
+  inserted: function (el, binding, vnode) {
     if (binding.value) { el.focus(); }
   },
-  updated: function (el, binding, vnode) {
+  update: function (el, binding, vnode) {
     if (binding.value && binding.value !== binding.oldValue) {
       el.focus();
     }
