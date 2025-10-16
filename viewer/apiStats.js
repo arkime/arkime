6d4d5bf0388e1f1a337de8e26b17d1913c350114
31453 <ecrinne@gmail.com>
2025-10-16 14:39:10 -0400

    remove unnecessary es nodes added to stats endpoint

--- viewer/apiStats.js
+++ viewer/apiStats.js
@@ -18,40 +18,6 @@ const ViewerUtils = require('./viewerUtils');
 
 class StatsAPIs {
   // --------------------------------------------------------------------------
-  // Helper Functions
-  // --------------------------------------------------------------------------
-  /**
-   * Extract ES node disk space information from nodesStats and nodesInfo.
-   * @param {object} nodesStats - The ES nodes stats response
-   * @param {object} nodesInfo - The ES nodes info response
-   * @returns {array} Array of ES node objects with disk space information
-   */
-  static buildESNodesList (nodesStats, nodesInfo) {
-    const esNodes = [];
-    const nodeKeys = Object.keys(nodesStats.nodes);
-
-    for (const nodeKey of nodeKeys) {
-      const node = nodesStats.nodes[nodeKey];
-
-      const ip = nodesInfo.nodes[nodeKey]?.ip;
-      const freeSize = node.roles.some(str => str.startsWith('data')) ? node.fs.total.available_in_bytes : 0;
-      const totalSize = node.roles.some(str => str.startsWith('data')) ? node.fs.total.total_in_bytes : 0;
-      const freeSpaceM = freeSize / 1000000;
-      const freeSpaceP = totalSize > 0 ? (freeSize / totalSize) * 100 : 0;
-
-      esNodes.push({
-        nodeName: node.name,
-        ip,
-        freeSpaceM,
-        freeSpaceP,
-        roles: node.roles
-      });
-    }
-
-    return esNodes;
-  }
-
-  // --------------------------------------------------------------------------
   // APIs
   // --------------------------------------------------------------------------
   // ES HEALTH APIS -----------------------------------------------------------
@@ -242,31 +208,10 @@ class StatsAPIs {
       const from = +req.query.start || 0;
       const stopLen = from + (+req.query.length || 500);
 
-      // Fetch ES node stats
-      Promise.all([
-        Db.nodesStatsCache(req.query.cluster),
-        Db.nodesInfoCache(req.query.cluster)
-      ]).then(([nodesStats, nodesInfo]) => {
-        const esNodes = StatsAPIs.buildESNodesList(nodesStats, nodesInfo);
-
-        const r = {
-          recordsTotal: total.count,
-          recordsFiltered: results.results.length,
-          data: results.results.slice(from, stopLen),
-          esNodes
-        };
-
-        res.send(r);
-      }).catch((esErr) => {
-        console.log(`ERROR - ${req.method} /api/stats ES nodes`, util.inspect(esErr, false, 50));
-        // Still send capture node stats even if ES stats fail
-        const r = {
-          recordsTotal: total.count,
-          recordsFiltered: results.results.length,
-          data: results.results.slice(from, stopLen),
-          esNodes: []
-        };
-        res.send(r);
+      res.send({
+        recordsTotal: total.count,
+        recordsFiltered: results.results.length,
+        data: results.results.slice(from, stopLen)
       });
     }).catch((err) => {
       console.log(`ERROR - ${req.method} /api/stats`, query, util.inspect(err, false, 50));
@@ -1652,7 +1597,7 @@ class StatsAPIs {
     Promise.all([
       Db.search('stats', 'stat', query),
       Db.numberOfDocuments('stats')
-    ]).then(([stats, total]) => {
+    ]).then(async ([stats, total]) => {
       if (stats.error) { throw stats.error; }
 
       const results = { total: stats.hits.total, results: [] };
@@ -1680,11 +1625,26 @@ class StatsAPIs {
       }
 
       // Fetch ES node stats
-      Promise.all([
-        Db.nodesStatsCache(req.query.cluster),
-        Db.nodesInfoCache(req.query.cluster)
-      ]).then(([nodesStats, nodesInfo]) => {
-        const esNodes = StatsAPIs.buildESNodesList(nodesStats, nodesInfo);
+      try {
+        const nodesStats = await Promise.all(Db.nodesStatsCache(req.query.cluster));
+        const esNodes = [];
+        const nodeKeys = Object.keys(nodesStats.nodes);
+
+        for (const nodeKey of nodeKeys) {
+          const node = nodesStats.nodes[nodeKey];
+
+          const freeSize = node.roles.some(str => str.startsWith('data')) ? node.fs.total.available_in_bytes : 0;
+          const totalSize = node.roles.some(str => str.startsWith('data')) ? node.fs.total.total_in_bytes : 0;
+          const freeSpaceM = freeSize / 1000000;
+          const freeSpaceP = totalSize > 0 ? (freeSize / totalSize) * 100 : 0;
+
+          esNodes.push({
+            nodeName: node.name,
+            freeSpaceM,
+            freeSpaceP,
+            roles: node.roles
+          });
+        }
 
         res.send({
           data: results.results,
@@ -1692,7 +1652,7 @@ class StatsAPIs {
           recordsFiltered: results.total,
           esNodes
         });
-      }).catch((esErr) => {
+      } catch (esErr) {
         console.log(`ERROR - ${req.method} /api/parliament ES nodes`, util.inspect(esErr, false, 50));
         // Still send capture node stats even if ES stats fail
         res.send({
@@ -1701,7 +1661,7 @@ class StatsAPIs {
           recordsFiltered: results.total,
           esNodes: []
         });
-      });
+      }
     }).catch((err) => {
       console.log(`ERROR - ${req.method} /api/parliament`, util.inspect(err, false, 50));
       res.send({ recordsTotal: 0, recordsFiltered: 0, data: [], esNodes: [] });
