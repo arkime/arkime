4f9a4b0c7347d681e1d09efa675d2efe0807ddd9
Andy Wick <andywick@amazon.com>
2026-01-30 09:34:06 -0500

    Add turn support

--- capture/parsers/stun.c
+++ capture/parsers/stun.c
@@ -2,8 +2,8 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  *
- * STUN (Session Traversal Utilities for NAT) parser
- * RFC 5389
+ * STUN (Session Traversal Utilities for NAT) and TURN parser
+ * RFC 5389, RFC 5766
  */
 #include "arkime.h"
 #include <arpa/inet.h>
@@ -15,6 +15,8 @@ LOCAL int mappedIpField;
 LOCAL int mappedPortField;
 LOCAL int xorMappedIpField;
 LOCAL int xorMappedPortField;
+LOCAL int xorRelayedIpField;
+LOCAL int xorRelayedPortField;
 LOCAL int usernameField;
 LOCAL int softwareField;
 LOCAL int realmField;
@@ -23,43 +25,63 @@ LOCAL int errorCodeField;
 // STUN magic cookie
 #define STUN_MAGIC_COOKIE 0x2112A442
 
-// STUN message types
+// STUN/TURN message types (method | class)
+// Class: 0x00=request, 0x10=indication, 0x100=success, 0x110=error
 #define STUN_BINDING_REQUEST        0x0001
 #define STUN_BINDING_RESPONSE       0x0101
 #define STUN_BINDING_ERROR          0x0111
-#define STUN_ALLOCATE_REQUEST       0x0003
-#define STUN_ALLOCATE_RESPONSE      0x0103
-#define STUN_ALLOCATE_ERROR         0x0113
+#define STUN_BINDING_INDICATION     0x0011
 
-// STUN attribute types
+// TURN message types
+#define TURN_ALLOCATE_REQUEST       0x0003
+#define TURN_ALLOCATE_RESPONSE      0x0103
+#define TURN_ALLOCATE_ERROR         0x0113
+#define TURN_REFRESH_REQUEST        0x0004
+#define TURN_REFRESH_RESPONSE       0x0104
+#define TURN_REFRESH_ERROR          0x0114
+#define TURN_SEND_INDICATION        0x0016
+#define TURN_DATA_INDICATION        0x0017
+#define TURN_CREATEPERM_REQUEST     0x0008
+#define TURN_CREATEPERM_RESPONSE    0x0108
+#define TURN_CREATEPERM_ERROR       0x0118
+#define TURN_CHANNELBIND_REQUEST    0x0009
+#define TURN_CHANNELBIND_RESPONSE   0x0109
+#define TURN_CHANNELBIND_ERROR      0x0119
+
+// STUN/TURN attribute types
 #define STUN_ATTR_MAPPED_ADDRESS    0x0001
 #define STUN_ATTR_USERNAME          0x0006
 #define STUN_ATTR_ERROR_CODE        0x0009
+#define STUN_ATTR_LIFETIME          0x000D
+#define STUN_ATTR_XOR_PEER_ADDRESS  0x0012
+#define STUN_ATTR_DATA              0x0013
 #define STUN_ATTR_REALM             0x0014
+#define STUN_ATTR_NONCE             0x0015
+#define STUN_ATTR_XOR_RELAYED_ADDR  0x0016
+#define STUN_ATTR_REQ_TRANSPORT     0x0019
 #define STUN_ATTR_XOR_MAPPED_ADDR   0x0020
 #define STUN_ATTR_SOFTWARE          0x8022
 #define STUN_ATTR_XOR_MAPPED_ADDR2  0x8020  // Legacy
 
-/******************************************************************************/
-LOCAL const char *stun_message_type_str(uint16_t type)
-{
-    switch (type) {
-    case STUN_BINDING_REQUEST:
-        return "Binding Request";
-    case STUN_BINDING_RESPONSE:
-        return "Binding Response";
-    case STUN_BINDING_ERROR:
-        return "Binding Error";
-    case STUN_ALLOCATE_REQUEST:
-        return "Allocate Request";
-    case STUN_ALLOCATE_RESPONSE:
-        return "Allocate Response";
-    case STUN_ALLOCATE_ERROR:
-        return "Allocate Error";
-    default:
-        return NULL;
-    }
-}
+// Message type lookup table indexed by method (0-9) and class (0-3)
+// Class: 0=request, 1=indication, 2=success response, 3=error response
+LOCAL const char *stunMethods[10] = {
+    [0] = NULL,
+    [1] = "Binding",
+    [3] = "Allocate",
+    [4] = "Refresh",
+    [6] = "Send",
+    [7] = "Data",
+    [8] = "CreatePermission",
+    [9] = "ChannelBind"
+};
+
+LOCAL const char *stunClasses[4] = {
+    "Request",
+    "Indication",
+    "Response",
+    "Error"
+};
 
 /******************************************************************************/
 LOCAL void stun_parse_address(ArkimeSession_t *session, BSB *bsb, int attrLen,
@@ -152,8 +174,13 @@ LOCAL int stun_parser(ArkimeSession_t *session, void *UNUSED(uw), const uint8_t 
             return 0;
     }
 
-    const char *typeStr = stun_message_type_str(msgType);
-    if (typeStr) {
+    // Add message type using method/class lookup
+    // Method: bits 0-3, 5-8, 11; Class: bits 4, 9
+    int method = ((msgType & 0x000F) | ((msgType & 0x00E0) >> 1) | ((msgType & 0x3E00) >> 2));
+    int msgClass = ((msgType & 0x0010) >> 4) | ((msgType & 0x0100) >> 7);
+    if (method > 0 && method < (int)ARRAY_LEN(stunMethods) && stunMethods[method]) {
+        char typeStr[64];
+        snprintf(typeStr, sizeof(typeStr), "%s %s", stunMethods[method], stunClasses[msgClass]);
         arkime_field_string_add(messageTypeField, session, typeStr, -1, TRUE);
     }
 
@@ -180,6 +207,11 @@ LOCAL int stun_parser(ArkimeSession_t *session, void *UNUSED(uw), const uint8_t 
         case STUN_ATTR_XOR_MAPPED_ADDR:
         case STUN_ATTR_XOR_MAPPED_ADDR2:
             stun_parse_address(session, &attrBsb, attrLen, xorMappedIpField, xorMappedPortField,
+                               STUN_MAGIC_COOKIE, txnId);
+            break;
+
+        case STUN_ATTR_XOR_RELAYED_ADDR:
+            stun_parse_address(session, &attrBsb, attrLen, xorRelayedIpField, xorRelayedPortField,
                                STUN_MAGIC_COOKIE, txnId);
             break;
 
@@ -242,11 +274,52 @@ LOCAL void stun_classify(ArkimeSession_t *session, const uint8_t *data, int len,
             return;
     }
 
+    arkime_session_add_protocol(session, "stun");
+    arkime_parsers_register(session, stun_parser, NULL, NULL);
+}
+
+/******************************************************************************/
+LOCAL int stun_tcp_parser(ArkimeSession_t *session, void *uw, const uint8_t *data, int len, int which)
+{
+    ArkimeParserBuf_t *pb = uw;
+
+    if (arkime_parser_buf_add(pb, which, data, len) < 0) {
+        arkime_parsers_unregister(session, pb);
+        return 0;
+    }
+
+    while (pb->len[which] >= 20) {
+        uint16_t msgLen = (pb->buf[which][2] << 8) | pb->buf[which][3];
+
+        if (msgLen + 20 > (int)sizeof(pb->buf[0]) || pb->len[which] < msgLen + 20)
+            return 0;
+
+        stun_parser(session, NULL, pb->buf[which], msgLen + 20, which);
+        arkime_parser_buf_del(pb, which, msgLen + 20);
+    }
+    return 0;
+}
+
+/******************************************************************************/
+LOCAL void stun_tcp_classify(ArkimeSession_t *session, const uint8_t *data, int len, int UNUSED(which), void *UNUSED(uw))
+{
+    if (len < 20)
+        return;
+
     if (arkime_session_has_protocol(session, "stun"))
         return;
 
+    // Check for STUN magic cookie at offset 4
+    if (memcmp(data + 4, "\x21\x12\xa4\x42", 4) != 0)
+        return;
+
+    // Verify first 2 bits are 0 (required for STUN)
+    if ((data[0] & 0xC0) != 0)
+        return;
+
     arkime_session_add_protocol(session, "stun");
-    arkime_parsers_register(session, stun_parser, NULL, NULL);
+    ArkimeParserBuf_t *pb = arkime_parser_buf_create();
+    arkime_parsers_register(session, stun_tcp_parser, pb, arkime_parser_buf_session_free);
 }
 
 /******************************************************************************/
@@ -319,10 +392,31 @@ void arkime_parser_init()
                                          ARKIME_FIELD_TYPE_INT_GHASH, ARKIME_FIELD_FLAG_CNT,
                                          (char *)NULL);
 
+    xorRelayedIpField = arkime_field_define("stun", "ip",
+                                            "stun.xor-relayed-ip", "XOR Relayed IP", "stun.xorRelayedIp",
+                                            "TURN XOR-RELAYED-ADDRESS IP (relay server allocated address)",
+                                            ARKIME_FIELD_TYPE_IP_GHASH, ARKIME_FIELD_FLAG_CNT,
+                                            (char *)NULL);
+
+    xorRelayedPortField = arkime_field_define("stun", "integer",
+                                              "stun.xor-relayed-port", "XOR Relayed Port", "stun.xorRelayedPort",
+                                              "TURN XOR-RELAYED-ADDRESS port",
+                                              ARKIME_FIELD_TYPE_INT_GHASH, ARKIME_FIELD_FLAG_CNT,
+                                              (char *)NULL);
+
     // UDP classifiers for binary STUN
     arkime_parsers_classifier_register_udp("stun", NULL, 0, (uint8_t *)"\x00\x01\x00", 3, stun_classify);
     arkime_parsers_classifier_register_udp("stun", NULL, 0, (uint8_t *)"\x00\x03\x00", 3, stun_classify);
     arkime_parsers_classifier_register_udp("stun", NULL, 0, (uint8_t *)"\x01\x01\x00", 3, stun_classify);
+
+    // TCP classifiers for binary STUN/TURN
+    arkime_parsers_classifier_register_tcp("stun", NULL, 0, (uint8_t *)"\x00\x01\x00", 3, stun_tcp_classify);
+    arkime_parsers_classifier_register_tcp("stun", NULL, 0, (uint8_t *)"\x00\x03\x00", 3, stun_tcp_classify);
+    arkime_parsers_classifier_register_tcp("stun", NULL, 0, (uint8_t *)"\x00\x04\x00", 3, stun_tcp_classify);
+    arkime_parsers_classifier_register_tcp("stun", NULL, 0, (uint8_t *)"\x00\x08\x00", 3, stun_tcp_classify);
+    arkime_parsers_classifier_register_tcp("stun", NULL, 0, (uint8_t *)"\x00\x09\x00", 3, stun_tcp_classify);
+    arkime_parsers_classifier_register_tcp("stun", NULL, 0, (uint8_t *)"\x01\x01\x00", 3, stun_tcp_classify);
+    arkime_parsers_classifier_register_tcp("stun", NULL, 0, (uint8_t *)"\x01\x03\x00", 3, stun_tcp_classify);
 
     // UDP/TCP classifiers for text RSP format
     arkime_parsers_classifier_register_udp("stun", NULL, 0, (uint8_t *)"RSP/", 4, stun_rsp_classify);
